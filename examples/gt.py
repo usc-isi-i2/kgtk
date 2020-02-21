8384ec79a58d448832eb84fc2812cc8ae3e936f4
Filip Ilievski <filip.dbrsk@gmail.com>
2020-02-21 13:45:29 -0800

    moved graph-tool functions from mowgli-in-the-jungle

---
+++ examples/gt.py
@@ -0,0 +1,12 @@
+import gt.io_utils as gtio
+import gt.analysis_utils as gtanalysis
+
+#datadir='data/'
+mowgli_nodes=f'{datadir}nodes_v002.csv'
+mowgli_edges=f'{datadir}edges_v002.csv'
+output_gml=f'{datadir}graph.graphml'
+
+gtio.transform_to_graphtool_format(mowgli_nodes, mowgli_edges, output_gml, True)
+g=gtio.load_gt_graph(output_gml.replace(".graphml", '.gt'))
+
+print(g.num_edges())
---
+++ gt/query_utils.py
@@ -0,0 +1,21 @@
+import graph_tool as gtmain
+import graph_tool.all as gtall
+
+def get_nodes_with_degree(g, deg_from, deg_to):
+    u = gtmain.GraphView(g, vfilt=lambda v: v.in_degree()+v.out_degree() in range(deg_from,deg_to))
+    return u
+
+def get_nodes_by_node_prop(g, p, v):
+    return gtall.find_vertex(g, prop=g.properties[('v', p)], match=v)
+
+def get_edges_by_edge_prop(g, p, v):
+    return gtall.find_edge(g, prop=g.properties[('e', p)], match=v)
+
+def get_neighbors(g, node_id, direction):
+    the_node=get_nodes_by_node_prop(g, '_graphml_vertex_id', node_id)[0]
+    if direction=='out':
+        return set(the_node.out_neighbors())
+    elif direction=='in':
+        return set(the_node.in_neighbors())
+    else: # total
+        return set(the_node.out_neighbors()) | set(the_node.in_neighbors())
