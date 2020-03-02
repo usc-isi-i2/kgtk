fbb41e2ed392675c65ee39faa10db1b39c3c93cf
Filip Ilievski <filip.dbrsk@gmail.com>
2020-03-02 13:42:38 -0800

    reorganizing and setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open('requirements.txt', 'r') as f:
+    install_requires = list()
+    dependency_links = list()
+    for line in f:
+        re = line.strip()
+        if re:
+            if re.startswith('git+') or re.startswith('svn+') or re.startswith('hg+'):
+                dependency_links.append(re)
+            else:
+                install_requires.append(re)
+
+packages = find_packages()
+
+setup(
+    name='kgtk',
+    version="0.1.0",
+    packages=packages,
+    url='https://github.com/usc-isi-i2/kgtk',
+    license='MIT',
+    author='ISI CKGs',
+    include_package_data=True,
+    install_requires=install_requires,
+    dependency_links=dependency_links
+)
