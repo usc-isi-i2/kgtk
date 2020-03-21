ddb5b3efab7a1c1c3237d1ca93a02fec6d5d5193
greatyyx <bigyyx@gmail.com>
2020-03-20 18:21:08 -0700

    avoid global import

--- kgtk/cli/dummy.py
+++ kgtk/cli/dummy.py
@@ -1,5 +1,7 @@
 """
 Example CLI module
+
+Please DON'T import modules globally, import them in `run`.
 """
 
 
@@ -20,15 +22,17 @@ def add_arguments(parser):
     Args:
         parser (argparse.ArgumentParser)
     """
-    parser.add_argument(action="store", type=str, metavar="test", dest="test")
+    parser.add_argument(action="store", type=str, metavar="name", dest="name")
     parser.add_argument("-i", "--info", action="store", type=str, dest="info")
 
 
-def run(test, info):
+def run(name, info):
     """
     Arguments here should be defined in `add_arguments` first.
     The return value (integer) will be the return code in shell. It will set to 0 if no value returns.
     You can either return a non-zero value to indicate error, or raise exceptions defined in kgtk.exceptions.
     """
-    print(test)
-    print(info)
+    # import modules locally
+    import socket
+
+    print('name: {}, info: {}\nhost: {}'.format(name, info, socket.gethostname()))
