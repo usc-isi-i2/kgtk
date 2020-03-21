6d3f31cf74ceac695dc9c46da8ef025eb92d84a7
Filip Ilievski <filip.dbrsk@gmail.com>
2020-03-20 23:33:47 -0700

    import csv and zcat

---
+++ kgtk/cli/import_csv.py
@@ -0,0 +1,25 @@
+"""
+Import CSV file module
+"""
+
+
+def parser():
+    return {
+        'help': 'this is a function to import CSV file'
+    }
+
+
+def add_arguments(parser):
+    """
+    Parse arguments
+    Args:
+        parser (argparse.ArgumentParser)
+    """
+    parser.add_argument(action="store", type=str, dest="filename", metavar='filename', help='filename here')
+    parser.add_argument("-d", "--delimiter", action="store", type=str, dest="delimiter")
+    parser.add_argument("-e", "--header", action="store", type=str, dest="delimiter")
+
+def run(filename, delimiter):
+    # import modules locally
+    import socket
+    print(f'Now importing {filename} with a delimiter {delimiter} and header {header}')
