3969d7d10720181aeb6cc49bcde9a41b006f32ae
filievski <filip.dbrsk@gmail.com>
2020-04-09 16:53:50 -0700

    import tsv and remove columns

--- kgtk/cli/import_tsv.py
+++ kgtk/cli/import_tsv.py
@@ -17,11 +17,11 @@ def add_arguments(parser):
     """
     parser.add_argument(action="store", type=str, dest="filename", metavar='filename', help='filename here')
     parser.add_argument("--header", action="store_true", dest="header_bool", help="Does the file contain a header in its first row")
-    parser.add_argument("-s", "--subject", action="store", type=int, dest="sub", help='Column in which the subject is given, default 0', default=0)
-    parser.add_argument("-o", "--object", action="store", type=int, dest="obj", help='Column in which the subject is given, default 2', default=2)
-    parser.add_argument('-p', '--props', action='store', type=str, dest="props", help="Edge properties to store in their order of appearance - comma-separated string.")
+    parser.add_argument("--subj", action="store", type=int, dest="sub", help='Column in which the subject is given, default 0', default=0)
+    parser.add_argument("--obj", action="store", type=int, dest="obj", help='Column in which the subject is given, default 2', default=2)
+    parser.add_argument('--pred', action='store', type=str, dest="props", help="Edge properties to store in their order of appearance - comma-separated string.")
     parser.add_argument('--directed', action='store_true', dest="directed", help="Is the graph directed or not?")
-    parser.add_argument('-out', '--output', action='store', type=str, dest='output', help='Graph tool file to dump the graph too - if empty, it will not be saved.')
+    parser.add_argument('-o', '--out', action='store', type=str, dest='output', help='Graph tool file to dump the graph too - if empty, it will not be saved.')
 
 def run(filename, header_bool, sub, obj, props, directed, output):
     # import modules locally
