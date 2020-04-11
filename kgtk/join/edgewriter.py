e6dfa1c0eb998cc4d0a0ce568498d0dad85f18c4
Craig Milo Rogers <rogers@isi.edu>
2020-04-11 15:42:12 -0700

    Initial edge file reader and writer.

--- kgtk/join/edgewriter.py
+++ kgtk/join/edgewriter.py
@@ -16,7 +16,7 @@ import typing
 # This helper class supports running gzip in parallel.
 #
 # TODO: can we use attrs here?
-class GunzipProcess(Process):
+class GzipProcess(Process):
     gzip_file: typing.TextIO = attr.ib() # Todo: validate TextIO
 
     # The line queue contains str with None as a plug.
@@ -30,39 +30,32 @@ class GunzipProcess(Process):
         self.line_queue = line_queue
 
     def run(self):
-        line: str
-        for line in self.gzip_file:
-            self.line_queue.put(line)
-        self.line_queue.put(None) # Plug the queue.
+        while True:
+            line: typing.Optional[str] =  self.line_queue.get()
+            if line is None: # This is the plug.
+                self.gzip_file.close()
+                return # Exit the process.
+            gzip_file.write(line)
 
-    # This is an iterator object.
-    def __iter__(self)-> typing.Iterator:
-        return self
-    
-    def __next__(self)->str:
-        line: typing.Optional[str] = self.line_queue.get()
-        if line is None: # Have we reached the plug?
-            raise StopIteration
-        else:
-            return line
+    # Called from the parent process.
+    def write(self, line: str):
+        self.line_queue.put(line)
+
+    # Called from the parent process.
+    def close(self):
+        self.line_queue.put(None) # Send the plug.
+        self.join() # Wait for the plug to exit the process.
 
 @attr.s(slots=True, frozen=True)
-class EdgeReader:
+class EdgeWriter:
     file_path: typing.Optional[Path] = attr.ib(validator=attr.validators.optional(attr.validators.instance_of(Path)))
-    file_in: typing.TextIO = attr.ib() # Todo: validate TextIO
+    file_out: typing.TextIO = attr.ib() # Todo: validate TextIO
     column_separator: str = attr.ib(validator=attr.validators.instance_of(str))
     column_names: typing.List[str] = attr.ib(validator=attr.validators.deep_iterable(member_validator=attr.validators.instance_of(str),
                                                                                      iterable_validator=attr.validators.instance_of(list)))
-    column_name_map: typing.Mapping[str, int] = attr.ib(validator=attr.validators.deep_mapping(key_validator=attr.validators.instance_of(str),
-                                                                                               value_validator=attr.validators.instance_of(int)))
 
     # For convenience, the count of columns. This is the same as len(column_names).
     column_count: int = attr.ib(validator=attr.validators.instance_of(int))
-
-    # The indices of the three mandatory columns:
-    node1_column_idx: int = attr.ib(validator=attr.validators.instance_of(int))
-    node2_column_idx: int = attr.ib(validator=attr.validators.instance_of(int))
-    label_column_idx: int = attr.ib(validator=attr.validators.instance_of(int))
 
     # Require or fill trailing fields?
     require_all_columns: bool = attr.ib(validator=attr.validators.instance_of(bool))
@@ -71,7 +64,7 @@ class EdgeReader:
 
     # Other implementation options?
     gzip_in_parallel: bool = attr.ib(validator=attr.validators.instance_of(bool))
-    gzip_thread: typing.Optional[GunzipProcess] = attr.ib(validator=attr.validators.optional(attr.validators.instance_of(GunzipProcess)))
+    gzip_thread: typing.Optional[GzipProcess] = attr.ib(validator=attr.validators.optional(attr.validators.instance_of(GzipProcess)))
     gzip_queue_size: int = attr.ib(validator=attr.validators.instance_of(int))
 
     # When we report line numbers in error messages, line 1 is the first line after the header line.
@@ -84,7 +77,6 @@ class EdgeReader:
     verbose: bool = attr.ib(validator=attr.validators.instance_of(bool))
     very_verbose: bool = attr.ib(validator=attr.validators.instance_of(bool))
 
-
     # TODO: There must be some place to import these constants
     NODE1_COLUMN_NAME: str = "node1"
     NODE2_COLUMN_NAME: str = "node2"
@@ -94,6 +86,7 @@ class EdgeReader:
 
     @classmethod
     def open(cls,
+             column_names: typing.List[str],
              file_path: typing.Optional[Path],
              require_all_columns: bool = True,
              prohibit_extra_columns: bool = True,
@@ -102,12 +95,13 @@ class EdgeReader:
              gzip_queue_size: int = GZIP_QUEUE_SIZE_DEFAULT,
              column_separator: str = "\t",
              verbose: bool = False,
-             very_verbose: bool = False)->"EdgeReader":
+             very_verbose: bool = False)->"EdgeWriter":
         if file_path is None or str(file_path) == "-":
             if verbose:
-                print("EdgeReader: reading stdin")
-            return cls._setup(file_path=None,
-                              file_in=sys.stdin,
+                print("EdgeWriter: writing stdout")
+            return cls._setup(column_names=column_names,
+                              file_path=None,
+                              file_out=sys.stdout,
                               require_all_columns=require_all_columns,
                               prohibit_extra_columns=prohibit_extra_columns,
                               fill_missing_columns=fill_missing_columns,
@@ -122,12 +116,13 @@ class EdgeReader:
             print("File_path.suffix: %s" % file_path.suffix)
         if file_path.suffix == ".gz":
             if verbose:
-                print("EdgeReader: reading gzip %s" % str(file_path))
+                print("EdgeWriter: writing gzip %s" % str(file_path))
 
             # TODO: find a better way to coerce typing.IO[Any] to typing.TextIO
-            gzip_file: typing.TextIO = gzip.open(file_path, mode="rt") # type: ignore
-            return cls._setup(file_path=file_path,
-                              file_in=gzip_file,
+            gzip_file: typing.TextIO = gzip.open(file_path, mode="wt") # type: ignore
+            return cls._setup(column_names=column_names,
+                              file_path=file_path,
+                              file_out=gzip_file,
                               require_all_columns=require_all_columns,
                               prohibit_extra_columns=prohibit_extra_columns,
                               fill_missing_columns=fill_missing_columns,
@@ -140,9 +135,10 @@ class EdgeReader:
             
         else:
             if verbose:
-                print("EdgeReader: reading file %s" % str(file_path))
-            return cls._setup(file_path=file_path,
-                              file_in=open(file_path, "r"),
+                print("EdgeWriter: writing file %s" % str(file_path))
+            return cls._setup(column_names=column_names,
+                              file_path=file_path,
+                              file_out=open(file_path, "w"),
                               require_all_columns=require_all_columns,
                               prohibit_extra_columns=prohibit_extra_columns,
                               fill_missing_columns=fill_missing_columns,
@@ -155,8 +151,9 @@ class EdgeReader:
     
     @classmethod
     def _setup(cls,
+               column_names: typing.List[str],
                file_path: typing.Optional[Path],
-               file_in: typing.TextIO,
+               file_out: typing.TextIO,
                require_all_columns: bool,
                prohibit_extra_columns: bool,
                fill_missing_columns: bool,
@@ -165,67 +162,38 @@ class EdgeReader:
                column_separator: str,
                verbose: bool = False,
                very_verbose: bool = False,
-    )->"EdgeReader":
-        """
-        Read the edge file header and split it into column names. Locate the three essential comumns.
-        """
-        # Read the column names from the first line.
-        header: str = file_in.readline()
-        if verbose:
-            print("header: %s" % header)
-        #
-        # TODO: if the read fails, throw a useful exception.
+    )->"EdgeWriter":
 
-        # Split the first line into column names.
-        column_names: typing.List[str] = header.split(column_separator)
-        if len(column_names) < 3:
-            # TODO: throw a better exception
-            raise ValueError("The edge file header must have at least three columns.")
-
-        # Validate the column names and build a map from column name
-        # to column index.
-        column_name_map: typing.MutableMapping[str, int] = { }
-        column_idx: int = 0 # There may be a more pythonic way to do this
-        column_name: str
-        for column_name in column_names:
-            if column_name is None or len(column_name) == 0:
-                # TODO: throw a better exception
-                raise ValueError("Invalid column name in the edge file header")
-            column_name_map[column_name] = column_idx
-            column_idx += 1
-
-        if EdgeReader.NODE1_COLUMN_NAME not in column_name_map:
+        # Validate the column names.
+        if EdgeWriter.NODE1_COLUMN_NAME not in column_names:
             # TODO: throw a better exception
             raise ValueError("Missing node1 column in the edge file header")
-        else:
-            node1_column_idx: int = column_name_map[EdgeReader.NODE1_COLUMN_NAME]
 
-        if EdgeReader.NODE2_COLUMN_NAME not in column_name_map:
+        if EdgeWriter.NODE2_COLUMN_NAME not in column_names:
             # TODO: throw a better exception
             raise ValueError("Missing node2 column in the edge file header")
-        else:
-            node2_column_idx: int = column_name_map[EdgeReader.NODE2_COLUMN_NAME]
 
-        if EdgeReader.LABEL_COLUMN_NAME not in column_name_map:
+        if EdgeWriter.LABEL_COLUMN_NAME not in column_names:
             # TODO: throw a better exception
             raise ValueError("Missing label column in the edge file header")
-        else:
-            label_column_idx: int = column_name_map[EdgeReader.LABEL_COLUMN_NAME]
 
-        gzip_thread: typing.Optional[GunzipProcess] = None
+        # Write the column names to the first line.
+        header: str = column_separator.join(column_names)
+        if verbose:
+            print("header: %s" % header)
+        file_out.write(header + "\n") # Todo: use system end-of-line sequence?
+
+
+        gzip_thread: typing.Optional[GzipProcess] = None
         if gzip_in_parallel:
-            gzip_thread = GunzipProcess(file_in, Queue(gzip_queue_size))
+            gzip_thread = GzipProcess(file_out, Queue(gzip_queue_size))
             gzip_thread.start()
 
         return cls(file_path=file_path,
-                   file_in=file_in,
+                   file_out=file_out,
                    column_separator=column_separator,
                    column_names=column_names,
-                   column_name_map=column_name_map,
                    column_count=len(column_names),
-                   node1_column_idx=node1_column_idx,
-                   node2_column_idx=node2_column_idx,
-                   label_column_idx=label_column_idx,
                    require_all_columns=require_all_columns,
                    prohibit_extra_columns=prohibit_extra_columns,
                    fill_missing_columns=fill_missing_columns,
@@ -237,27 +205,16 @@ class EdgeReader:
                    very_verbose=very_verbose,
         )
 
-    # This is an iterator object.
-    def __iter__(self)-> typing.Iterator:
-        return self
 
-    # Get the next edge values as a list of strings.
-    # TODO: Convert integers, coordinates, etc. to Python types
-    def __next__(self)-> typing.List[str]:
-        line: str
-        try:
-            if self.gzip_thread is not None:
-                line = next(self.gzip_thread) # TODO: unify this
-            else:
-                line = next(self.file_in) # Will throw StopIteration
-        except StopIteration as e:
-            # Close the input file!
-            #
-            # TODO: implement a close() routine and/or whatever it takes to support "with".
-            self.file_in.close() # Do we need to guard against repeating this call?
-            raise e
+    # Write the next list of edge values as a list of strings.
+    # TODO: Convert integers, coordinates, etc. from Python types
+    def write(self, values: typing.List[str]):
+        # Optionally fill missing trailing columns with empty values:
+        if self.fill_missing_columns and len(values) < self.column_count:
+            while len(values) < self.column_count:
+                values.append("")
 
-        values: typing.List[str] = line.split(self.column_separator)
+        line: str = self.column_separator.join(values)
 
         # Optionally validate that the line contained the right number of columns:
         #
@@ -268,14 +225,20 @@ class EdgeReader:
             raise ValueError("Required %d columns in input line %d, saw %d (%d extra): '%s'" % (self.column_count, self.line_count[0], len(values),
                                                                                                 len(values) - self.column_count, line))
 
-        # Optionally fill missing trailing columns with empty values:
-        if self.fill_missing_columns and len(values) < self.column_count:
-            while len(values) < self.column_count:
-                values.append("")
+        if self.gzip_thread is not None:
+            self.gzip_thread.write(line + "\n")
+        else:
+            self.file_out.write(line + "\n")
 
         self.line_count[0] += 1
         if self.very_verbose:
             sys.stdout.write(".")
             sys.stdout.flush()
-            
-        return values
+
+    def close(self):
+        if self.gzip_thread is not None:
+            self.gzip_thread.close()
+        else:
+            self.file_out.close()
+
+
