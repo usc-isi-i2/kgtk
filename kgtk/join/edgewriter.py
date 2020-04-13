57d38fb4252972ad21f6f31b1c8942c57de507f1
Craig Milo Rogers <rogers@isi.edu>
2020-04-12 18:24:05 -0700

    Move the gzip-in-a-process code to a seperate file.

--- kgtk/join/edgewriter.py
+++ kgtk/join/edgewriter.py
@@ -10,43 +10,10 @@ import gzip
 from pathlib import Path
 from multiprocessing import Queue
 import sys
-from multiprocessing import Process
 import typing
 
+from kgtk.join.gzipprocess import GzipProcess
 from kgtk.join.kgtk_format import KgtkFormat
-
-# This helper class supports running gzip in parallel.
-#
-# TODO: can we use attrs here?
-class GzipProcess(Process):
-    gzip_file: typing.TextIO = attr.ib() # Todo: validate TextIO
-
-    # The line queue contains str with None as a plug.
-    #
-    # TODO: can we do a better job of type declaration here?
-    line_queue: Queue = attr.ib(validator=attr.validators.instance_of(Queue))
-
-    def __init__(self,  gzip_file: typing.TextIO, line_queue: Queue):
-        super().__init__()
-        self.gzip_file = gzip_file
-        self.line_queue = line_queue
-
-    def run(self):
-        while True:
-            line: typing.Optional[str] =  self.line_queue.get()
-            if line is None: # This is the plug.
-                self.gzip_file.close()
-                return # Exit the process.
-            self.gzip_file.write(line)
-
-    # Called from the parent process.
-    def write(self, line: str):
-        self.line_queue.put(line)
-
-    # Called from the parent process.
-    def close(self):
-        self.line_queue.put(None) # Send the plug.
-        self.join() # Wait for the plug to exit the process.
 
 @attr.s(slots=True, frozen=True)
 class EdgeWriter:
