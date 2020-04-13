ae4b14390b3b89e1e30cdf1f85f31bb143287c79
Rongpeng <rongpenl@usc.edu>
2020-04-13 08:52:01 -0700

    build the wikidata props

--- examples/obtain_wikidata_properties.py
+++ examples/obtain_wikidata_properties.py
@@ -18,7 +18,8 @@ query = """SELECT ?property ?propertyType WHERE {
 
 
 def get_results(endpoint_url, query):
-    user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
+    user_agent = "WDQS-example Python/%s.%s" % (
+        sys.version_info[0], sys.version_info[1])
     # TODO adjust user agent; see https://w.wiki/CX6
     sparql = SPARQLWrapper(endpoint_url, agent=user_agent)
     sparql.setQuery(query)
@@ -26,9 +27,21 @@ def get_results(endpoint_url, query):
     return sparql.query().convert()
 
 
+dataTypeMappings = {
+    "WikibaseItem": "item",
+    "Time": "time",
+    "GlobeCoordinate": "globe-coordinate",
+    "Quantity": "quantity",
+    "Monolingualtext": "monolingualtext",
+    "String": "string",
+    "ExternalId":"external-identifier",
+    "Url":"url"
+}
 results = get_results(endpoint_url, query)
-print(results)
 
-with open("../data/wikidataProps.json","w") as fp:
-    json.dump(results,fp)
-
+with open("../data/wikidataProps.tsv", "w") as fp:
+    for prop in results["results"]["bindings"]:
+        pID = prop["property"]["value"].split("/")[-1]
+        pType = prop["propertyType"]["value"].split("#")[-1]
+        pType = dataTypeMappings.get(pType,"string")
+        fp.write(pID + "\tproperty_type\t" + pType+"\n")
