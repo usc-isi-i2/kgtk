57e927c257ada60f0a84a178f24c8ff30e2ed396
greatyyx <bigyyx@gmail.com>
2020-04-16 11:44:19 -0700

    support default arguments

--- kgtk/cli_argparse.py
+++ kgtk/cli_argparse.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
-from kgtk.exceptions import KGTKArgumentParseException
+from functools import partial
+from kgtk.exceptions import KGTKArgumentParseException, KGTKSyntaxException
 
 
 class KGTKArgumentParser(ArgumentParser):
@@ -8,11 +9,22 @@ class KGTKArgumentParser(ArgumentParser):
             kwargs['formatter_class'] = RawDescriptionHelpFormatter
 
         self.shared_arguments = set()
+        self.default_arguments = set()
+        self.add_default_argument_funcs = {}
 
         super(KGTKArgumentParser, self).__init__(*args, **kwargs)
 
-    def accept_shared_argument(self, dest_name):
-        self.shared_arguments.add(dest_name)
+    def add_default_argument(self, *args, **kwargs):
+        if 'dest' not in kwargs:
+            raise KGTKSyntaxException('Default argument error: dest not defined')
+        self.add_default_argument_funcs[kwargs['dest']] = partial(self.add_argument, *args, **kwargs)
+
+    def accept_shared_argument(self, dest):
+        self.shared_arguments.add(dest)
+
+    def accept_default_argument(self, dest):
+        # load default arguments
+        self.add_default_argument_funcs[dest]()
 
     def exit(self, status=0, message=None):
         if status == 2:
@@ -26,4 +38,11 @@ def add_shared_arguments(parser):
     # 2. need to have default value
     # 3. need to specify dest, which value starts with underscore
     # e.g., parser.add_argument('--debug', dest='_debug', action='store_true', default=False, help='enable debug mode')
+    pass
+
+
+def add_default_arguments(parser):
+    # set default arguments here
+    # need to specify dest
+    # e.g., parser.add_default_argument('--save', dest='save', action='store', help='save to file')
     pass
