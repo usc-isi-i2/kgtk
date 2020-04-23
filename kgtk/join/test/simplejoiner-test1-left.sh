c2d639d2a6991377908038944ec4f737bdce2807
Craig Milo Rogers <rogers@isi.edu>
2020-04-22 20:27:44 -0700

    Join edge files to edge files, node files to node files.

--- kgtk/join/test/simplejoiner-test1-left.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-python3 kgtk/join/simplejoiner.py \
-	kgtk/join/test/simplejoiner-test1-file1.tsv \
-	kgtk/join/test/simplejoiner-test1-file2.tsv \
-	--left-join \
-	--output-file kgtk/join/test/simplejoiner-test1-left-output.tsv
