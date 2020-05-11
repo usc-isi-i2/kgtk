26ec4b5c7e75d46e17c3878f8ab3692021d6f60d
Craig Milo Rogers <rogers@isi.edu>
2020-05-11 12:17:50 -0700

    Continuing conversion to KgtkReaderOptions.

--- kgtk/cli/ifnotexists.py
+++ kgtk/cli/ifnotexists.py
@@ -1,18 +1,16 @@
-"""Filter a KGTK file based on whether one or more records do not exist in a
-second KGTK file with matching values for one or more fields.
+"""Filter a KGTK file based on whether one or more records exist in a second
+KGTK file with matching values for one or more fields.
 
+TODO: Need KgtkWriterOptions
 """
 
 from pathlib import Path
 import sys
 import typing
 
-from kgtk.kgtkformat import KgtkFormat
-from kgtk.io.kgtkreader import KgtkReader
+from kgtk.io.kgtkreader import KgtkReader, KgtkReaderOptions
 from kgtk.io.kgtkwriter import KgtkWriter
 from kgtk.join.ifexists import IfExists
-from kgtk.utils.enumnameaction import EnumNameAction
-from kgtk.utils.validationaction import ValidationAction
 from kgtk.value.kgtkvalueoptions import KgtkValueOptions
 
 def parser():
@@ -27,34 +25,21 @@ def add_arguments(parser):
     Args:
         parser (argparse.ArgumentParser)
     """
-    parser.add_argument(      "input_kgtk_file", nargs="?", help="The KGTK file to filter ('left' file). May be omitted or '-' for stdin.", type=Path)
+    parser.add_argument(      "input_kgtk_file", nargs="?", help="The KGTK file to filter. May be omitted or '-' for stdin.", type=Path)
 
-    parser.add_argument(      "--filter-on", dest="filter_kgtk_file", help="The KGTK file to filter against ('right' file).", type=Path, required=True)
+    parser.add_argument(      "--filter-on", dest="_filter_kgtk_file", help="The KGTK file to filter against.", type=Path, required=True)
 
     parser.add_argument("-o", "--output-file", dest="output_kgtk_file", help="The KGTK file to write", type=Path, default=None)
 
-    parser.add_argument(      "--left-keys", dest="left_keys", help="The key columns in the file being filtered.", nargs='*')
+    parser.add_argument(      "--input-keys", "--left-keys", dest="input_keys", help="The key columns in the file being filtered.", nargs='*')
 
-    parser.add_argument(      "--right-keys", dest="right_keys", help="The key columns in the filter-on file.", nargs='*')
+    parser.add_argument(      "--filter-keys", "--right-keys", dest="filter_keys", help="The key columns in the filter-on file.", nargs='*')
 
+    parser.add_argument(      "--field-separator", dest="field_separator", help="Separator for multifield keys", default=IfExists.FIELD_SEPARATOR_DEFAULT)
 
-    # A subset of common arguments:
-    errors_to = parser.add_mutually_exclusive_group()
-    errors_to.add_argument(      "--errors-to-stdout", dest="errors_to_stdout",
-                              help="Send errors to stdout instead of stderr (default)", action="store_true")
-    errors_to.add_argument(      "--errors-to-stderr", dest="errors_to_stderr",
-                              help="Send errors to stderr instead of stdout", action="store_true")
-
-    parser.add_argument(      "--error-limit", dest="error_limit",
-                              help="The maximum number of errors to report before failing", type=int, default=KgtkReader.ERROR_LIMIT_DEFAULT)
-
-    parser.add_argument(      "--field-separator", dest="field_separator",
-                              help="Field separator.", type=str, default=IfExists.FIELD_SEPARATOR_DEFAULT)
-
-    parser.add_argument("-v", "--verbose", dest="verbose", help="Print additional progress messages.", action='store_true')
-    
-    parser.add_argument(      "--very-verbose", dest="very_verbose", help="Print additional progress messages.", action='store_true')
-    
+    KgtkReader.add_file_arguments(parser, mode_options=True, who="input")
+    KgtkReader.add_file_arguments(parser, mode_options=True, who="filter")
+    KgtkValueOptions.add_arguments(parser)
 
 
     # Note: Any arguments described by KgtkValueOptions.add_arguments(...)
@@ -65,45 +50,46 @@ def add_arguments(parser):
 def run(input_kgtk_file: typing.Optional[Path],
         filter_kgtk_file: Path,
         output_kgtk_file: typing.Optional[Path],
-        left_keys: typing.Optional[typing.List[str]],
-        right_keys: typing.Optional[typing.List[str]],
+        input_keys: typing.Optional[typing.List[str]],
+        filter_keys: typing.Optional[typing.List[str]],
         
-        # Some common arguments:
-        errors_to_stdout: bool = False,
-        errors_to_stderr: bool = False,
-        error_limit: int = KgtkReader.ERROR_LIMIT_DEFAULT,
         field_separator: str = IfExists.FIELD_SEPARATOR_DEFAULT,
+
+        errors_to_stdout: bool = False,
+        errors_to_stderr: bool = True,
         verbose: bool = False,
         very_verbose: bool = False,
 
-        **kwargs # Whatever KgtkValueOptions wants.
+        **kwargs # Whatever KgtkFileOptions and KgtkValueOptions want.
 )->int:
     # import modules locally
     from kgtk.exceptions import KGTKException
 
 
-    if input_kgtk_file is None:
-        input_kgtk_file = Path("-")
-
     # Select where to send error messages, defaulting to stderr.
-    # (Not used yet)
-    error_file: typing.TextIO = sys.stderr if errors_to_stderr else sys.stdout
+    error_file: typing.TextIO = sys.stdout if errors_to_stdout else sys.stderr
 
-    # Build the value parsing option structure.
+    # Build the option structures.
+    input_reader_options: KgtkReaderOptions = KgtkReaderOptions.from_dict(kwargs, who="input")
+    filter_reader_options: KgtkReaderOptions = KgtkReaderOptions.from_dict(kwargs, who="filter")
     value_options: KgtkValueOptions = KgtkValueOptions.from_dict(kwargs)
 
     try:
-        ie: IfExists = IfExists(left_file_path=input_kgtk_file,
-                                right_file_path=filter_kgtk_file,
-                                output_path=output_kgtk_file,
-                                invert=True,
-                                left_keys=left_keys,
-                                right_keys=right_keys,
-                                field_separator=field_separator,
-                                value_options=value_options,
-                                error_limit=error_limit,
-                                verbose=verbose,
-                                very_verbose=very_verbose)
+        ie: IfExists = IfExists(
+            input_file_path=input_kgtk_file,
+            input_keys=input_keys,
+            filter_file_path=filter_kgtk_file,
+            filter_keys=filter_keys,
+            output_file_path=output_kgtk_file,
+            invert=True,
+            field_separator=field_separator,
+            input_reader_options=input_reader_options,
+            filter_reader_options=filter_reader_options,
+            value_options=value_options,
+            error_file=error_file,
+            verbose=verbose,
+            very_verbose=very_verbose,
+        )
         
         ie.process()
 
