6c0c154b65929f644aa475ebfb601db9b8c00045
Craig Milo Rogers <rogers@isi.edu>
2020-05-12 12:38:19 -0700

    Ensure that all special column indexes are looked up.  Share code better between KgtkReader and its subclasses.

--- kgtk/io/nodereader.py
+++ kgtk/io/nodereader.py
@@ -10,7 +10,7 @@ from pathlib import Path
 import sys
 import typing
 
-from kgtk.io.kgtkreader import KgtkReader, KgtkReaderOptions
+from kgtk.io.kgtkreader import KgtkReader, KgtkReaderMode, KgtkReaderOptions
 from kgtk.utils.closableiter import ClosableIter
 from kgtk.utils.enumnameaction import EnumNameAction
 from kgtk.utils.validationaction import ValidationAction
@@ -28,50 +28,24 @@ class NodeReader(KgtkReader):
                        verbose: bool = False,
                        very_verbose: bool = False)->"NodeReader":
 
-        # Supply the default reader and value options:
-        (options, value_options) = cls._default_options(options, value_options)
-
-        source: ClosableIter[str] = cls._openfile(file_path, options=options, error_file=error_file, verbose=verbose)
-
-        # Read the edge file header and split it into column names.
-        header: str
-        column_names: typing.List[str]
-        (header, column_names) = cls._build_column_names(source, options=options, error_file=error_file, verbose=verbose)
-
-        # Check for unsafe column names.
-        cls.check_column_names(column_names,
-                               header_line=header,
-                               error_action=options.unsafe_column_name_action,
-                               error_file=error_file)
-
-        # Build a map from column name to column index.
-        column_name_map: typing.Mapping[str, int] = cls.build_column_name_map(column_names,
-                                                                              header_line=header,
-                                                                              error_action=options.header_error_action,
-                                                                              error_file=error_file)
-        # Get the index of the required column.
-        id_column_idx: int = cls.required_node_column(column_name_map,
-                                                      header_line=header,
-                                                      error_action=options.header_error_action,
-                                                      error_file=error_file)
-
-        if verbose:
-            print("NodeReader: Reading an node file. id=%d" % (id_column_idx))
-
-        return cls(file_path=file_path,
-                   source=source,
-                   column_names=column_names,
-                   column_name_map=column_name_map,
-                   column_count=len(column_names),
-                   id_column_idx=id_column_idx,
-                   error_file=error_file,
-                   options=options,
-                   value_options=value_options,
-                   is_edge_file=False,
-                   is_node_file=True,
-                   verbose=verbose,
-                   very_verbose=very_verbose,
-        )
+        result: KgtkReader = cls.open(file_path=file_path,
+                                      error_file=error_file,
+                                      mode=KgtkReaderMode.NODE,
+                                      options=options,
+                                      value_options=value_options,
+                                      verbose=verbose,
+                                      very_verbose=very_verbose)
+        # This doesn't work because the EdgeReader imported inside KgtkReader
+        # is a different class than this one!
+        #
+        # TODO: Fix this.
+        #
+        #if isinstance(result, cls):
+        #    return result
+        #else:
+        #    # TODO: throw a better exception
+        #    raise ValueError("open_node_file expected to produce a NodeReader")
+        return typing.cast(NodeReader, result)
 
     def _ignore_if_blank_required_fields(self, values: typing.List[str], line: str)->bool:
         # Ignore line_action with blank id fields.  This code comes after
@@ -105,11 +79,10 @@ def main():
     reader_options: KgtkReaderOptions = KgtkReaderOptions.from_args(args, mode=KgtkReaderMode.NODE)
     value_options: KgtkValueOptions = KgtkValueOptions.from_args(args)
 
-    nr: NodeReader = NodeReader.open_edge_file(args.kgtk_file,
+    nr: NodeReader = NodeReader.open_node_file(args.kgtk_file,
                                                error_file=error_file,
                                                options=reader_options,
                                                value_options=value_options,
-                                               column_separator=args.column_separator,
                                                verbose=args.verbose, very_verbose=args.very_verbose)
 
     line_count: int = 0
