6926a58fe773a92a7dd752607f6df411ce9eef17
Craig Milo Rogers <rogers@isi.edu>
2020-05-11 20:46:46 -0700

    Remove obsolete test.

--- kgtk/join/test/nodejoiner-test1-inner.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-python3 kgtk/join/nodejoiner.py \
-	kgtk/join/test/nodejoiner-test1-file1.tsv \
-	kgtk/join/test/nodejoiner-test1-file2.tsv \
-	--output-file kgtk/join/test/nodejoiner-test1-inner-output.tsv
