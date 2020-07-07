c25b8e24691f66a8da2637af092958623676d19b
Craig Milo Rogers <rogers@isi.edu>
2020-07-07 11:18:32 -0700

    Fix nested class reference error.

--- kgtk/value/propertypatternvalidator.py
+++ kgtk/value/propertypatternvalidator.py
@@ -40,6 +40,7 @@ class PropertyPattern:
     label_value: KgtkValue = attr.ib(validator=attr.validators.instance_of(KgtkValue))
     node2_value: KgtkValue = attr.ib(validator=attr.validators.instance_of(KgtkValue))
 
+    # TODO: create validators:
     action: Action = attr.ib()
     pattern: typing.Optional[typing.Pattern] = attr.ib()
     intval: typing.Optional[int] = attr.ib()
@@ -48,14 +49,14 @@ class PropertyPattern:
     value: str = attr.ib()
 
     @classmethod
-    def new(cls, node1_value: KgtkValue, label_value: KgtkValue, node2_value: KgtkValue)->'PropertyPatternRecord':
-        action: Action = Action(label_value.value)
+    def new(cls, node1_value: KgtkValue, label_value: KgtkValue, node2_value: KgtkValue)->'PropertyPattern':
+        action: cls.Action = cls.Action(label_value.value)
 
         pattern: typing.Optional[typing.Pattern] = None
-        if action in (Action.NODE1_PATTERN,
-                      Action.NODE2_PATTERN,
-                      Action.LABEL_PATTERN,
-                      Action.MATCHES):
+        if action in (cls.Action.NODE1_PATTERN,
+                      cls.Action.NODE2_PATTERN,
+                      cls.Action.LABEL_PATTERN,
+                      cls.Action.MATCHES):
             if node2_value.fields is None:
                 raise ValueError("%s: Node2 has no fields" % (action.value)) # TODO: better complaint
             if node2_value.fields.text is None:
@@ -64,10 +65,10 @@ class PropertyPattern:
             pattern = re.compile(node2_value.fields.text)
 
         intval: typing.Optional[int] = None
-        if action in (Action.MINOCCURS,
-                      Action.MAXOCCURS,
-                      Action.MINDISTINCT,
-                      Action.MAXDISTINCT):
+        if action in (cls.Action.MINOCCURS,
+                      cls.Action.MAXOCCURS,
+                      cls.Action.MINDISTINCT,
+                      cls.Action.MAXDISTINCT):
             if node2_value.fields is None:
                 raise ValueError("%s: Node2 has no fields" % (action.value)) # TODO: better complaint
             if node2_value.fields.number is None:
@@ -75,8 +76,8 @@ class PropertyPattern:
             intval = int(node2_value.fields.number)
 
         number: typing.Optional[float] = None
-        if action in(Action.MINVAL,
-                     Action.MAXVAL):
+        if action in(cls.Action.MINVAL,
+                     cls.Action.MAXVAL):
             if node2_value.fields is None:
                 raise ValueError("%s: Node2 has no fields" % (action.value)) # TODO: better complaint
             if node2_value.fields.number is None:
@@ -84,7 +85,7 @@ class PropertyPattern:
             number = float(node2_value.fields.number)
 
         column_names: typing.List[str] = [ ]
-        if action in (Action.NOT_IN,):
+        if action in (cls.Action.NOT_IN,):
             if label_value.is_symbol:
                 column_names.append(label_value.value)
             elif label_value.is_list:
@@ -98,10 +99,10 @@ class PropertyPattern:
                 raise ValueError("%s:Value is not a symbol or list of symbols" % (action.value)) # TODO: better complaint
             # TODO: validate that the column names are valid and get their indexes.
 
-        if action in (Action.LABEL_COLUM,
-                      Action.NODE1_COLUMN,
-                      Action.NODE2_COLUMN,
-                      Action.REQUIRED_IN):
+        if action in (cls.Action.LABEL_COLUM,
+                      cls.Action.NODE1_COLUMN,
+                      cls.Action.NODE2_COLUMN,
+                      cls.Action.REQUIRED_IN):
             if label_value.is_symbol:
                 column_names.append(label_value.value)
             else:
@@ -109,9 +110,9 @@ class PropertyPattern:
             # TODO: validate that the column names are valid and get their indexes.
 
         value: str = ""
-        if action in (Action.NODE1_TYPE,
-                      Action.NODE2_TYPE,
-                      Action.ISA):
+        if action in (cls.Action.NODE1_TYPE,
+                      cls.Action.NODE2_TYPE,
+                      cls.Action.ISA):
             value = node2_value.value
                         
 
