1ff4bc9e2c24f3093c35636107c5a035990ad692
Craig Milo Rogers <rogers@isi.edu>
2020-07-07 11:02:30 -0700

    Rename and restruture the property pattern validator.

--- kgtk/value/propertypatterns.py
+++ kgtk/value/propertypatterns.py
@@ -1,5 +1,5 @@
 """
-Validate datatypes.
+Validate property patterns..
 """
 
 from argparse import ArgumentParser, Namespace
@@ -10,39 +10,37 @@ import typing
 
 from kgtk.value.kgtkvalue import KgtkValue
 
-class ValidateDatatypesAction(Enum):
-    NODE1_TYPE = "node1_type"
-    NODE2_TYPE = "node2_type"
-    NOT_IN = "not_in"
-    LABEL_COLUM = "label_column"
-    NODE1_COLUMN = "node1_column"
-    NODE2_COLUMN = "node2_column"
-    NODE1_VALUES = "node1_values"
-    NODE2_VALUES = "node2_values"
-    MINVAL = "minval"
-    MAXVAL = "maxval"
-    MINOCCURS = "minoccurs"
-    MAXOCCURS = "maxoccurs"
-    ISA = "isa"
-    MATCHES = "matches"
-    NODE1_PATTERN = "node1_pattern"
-    NODE2_PATTERN = "node2_pattern"
-    LABEL_PATTERN = "label_pattern"
-    MINDISTINCT = "mindistinct"
-    MAXDISTINCT = "maxdistinct"
-    REQUIRED_IN = "required_id"
-    MINDATE = "mindate"
-    MAXDATE = "maxdate"
-
-    
-        
 @attr.s(slots=True, frozen=True)
-class ValidateDatatypesRecord:
+class PropertyPattern:
+    class Action(Enum):
+        NODE1_TYPE = "node1_type"
+        NODE2_TYPE = "node2_type"
+        NOT_IN = "not_in"
+        LABEL_COLUM = "label_column"
+        NODE1_COLUMN = "node1_column"
+        NODE2_COLUMN = "node2_column"
+        NODE1_VALUES = "node1_values"
+        NODE2_VALUES = "node2_values"
+        MINVAL = "minval"
+        MAXVAL = "maxval"
+        MINOCCURS = "minoccurs"
+        MAXOCCURS = "maxoccurs"
+        ISA = "isa"
+        MATCHES = "matches"
+        NODE1_PATTERN = "node1_pattern"
+        NODE2_PATTERN = "node2_pattern"
+        LABEL_PATTERN = "label_pattern"
+        MINDISTINCT = "mindistinct"
+        MAXDISTINCT = "maxdistinct"
+        REQUIRED_IN = "required_id"
+        MINDATE = "mindate"
+        MAXDATE = "maxdate"
+        
     node1_value: KgtkValue = attr.ib(validator=attr.validators.instance_of(KgtkValue))
     label_value: KgtkValue = attr.ib(validator=attr.validators.instance_of(KgtkValue))
     node2_value: KgtkValue = attr.ib(validator=attr.validators.instance_of(KgtkValue))
 
-    action: ValidateDatatypesAction = attr.ib()
+    action: Action = attr.ib()
     pattern: typing.Optional[typing.Pattern] = attr.ib()
     intval: typing.Optional[int] = attr.ib()
     number: typing.Optional[float] = attr.ib()
@@ -50,14 +48,14 @@ class ValidateDatatypesRecord:
     value: str = attr.ib()
 
     @classmethod
-    def new(cls, node1_value: KgtkValue, label_value: KgtkValue, node2_value: KgtkValue)->'ValidateDatatypesRecord':
-        action: ValidateDatatypesAction = ValidateDatatypesAction(label_value.value)
+    def new(cls, node1_value: KgtkValue, label_value: KgtkValue, node2_value: KgtkValue)->'PropertyPatternRecord':
+        action: Action = Action(label_value.value)
 
         pattern: typing.Optional[typing.Pattern] = None
-        if action in (ValidateDatatypesAction.NODE1_PATTERN,
-                      ValidateDatatypesAction.NODE2_PATTERN,
-                      ValidateDatatypesAction.LABEL_PATTERN,
-                      ValidateDatatypesAction.MATCHES):
+        if action in (Action.NODE1_PATTERN,
+                      Action.NODE2_PATTERN,
+                      Action.LABEL_PATTERN,
+                      Action.MATCHES):
             if node2_value.fields is None:
                 raise ValueError("%s: Node2 has no fields" % (action.value)) # TODO: better complaint
             if node2_value.fields.text is None:
@@ -66,10 +64,10 @@ class ValidateDatatypesRecord:
             pattern = re.compile(node2_value.fields.text)
 
         intval: typing.Optional[int] = None
-        if action in (ValidateDatatypesAction.MINOCCURS,
-                      ValidateDatatypesAction.MAXOCCURS,
-                      ValidateDatatypesAction.MINDISTINCT,
-                      ValidateDatatypesAction.MAXDISTINCT):
+        if action in (Action.MINOCCURS,
+                      Action.MAXOCCURS,
+                      Action.MINDISTINCT,
+                      Action.MAXDISTINCT):
             if node2_value.fields is None:
                 raise ValueError("%s: Node2 has no fields" % (action.value)) # TODO: better complaint
             if node2_value.fields.number is None:
@@ -77,8 +75,8 @@ class ValidateDatatypesRecord:
             intval = int(node2_value.fields.number)
 
         number: typing.Optional[float] = None
-        if action in(ValidateDatatypesAction.MINVAL,
-                     ValidateDatatypesAction.MAXVAL):
+        if action in(Action.MINVAL,
+                     Action.MAXVAL):
             if node2_value.fields is None:
                 raise ValueError("%s: Node2 has no fields" % (action.value)) # TODO: better complaint
             if node2_value.fields.number is None:
@@ -86,7 +84,7 @@ class ValidateDatatypesRecord:
             number = float(node2_value.fields.number)
 
         column_names: typing.List[str] = [ ]
-        if action in (ValidateDatatypesAction.NOT_IN,):
+        if action in (Action.NOT_IN,):
             if label_value.is_symbol:
                 column_names.append(label_value.value)
             elif label_value.is_list:
@@ -100,10 +98,10 @@ class ValidateDatatypesRecord:
                 raise ValueError("%s:Value is not a symbol or list of symbols" % (action.value)) # TODO: better complaint
             # TODO: validate that the column names are valid and get their indexes.
 
-        if action in (ValidateDatatypesAction.LABEL_COLUM,
-                      ValidateDatatypesAction.NODE1_COLUMN,
-                      ValidateDatatypesAction.NODE2_COLUMN,
-                      ValidateDatatypesAction.REQUIRED_IN):
+        if action in (Action.LABEL_COLUM,
+                      Action.NODE1_COLUMN,
+                      Action.NODE2_COLUMN,
+                      Action.REQUIRED_IN):
             if label_value.is_symbol:
                 column_names.append(label_value.value)
             else:
@@ -111,14 +109,14 @@ class ValidateDatatypesRecord:
             # TODO: validate that the column names are valid and get their indexes.
 
         value: str = ""
-        if action in (ValidateDatatypesAction.NODE1_TYPE,
-                      ValidateDatatypesAction.NODE2_TYPE,
-                      ValidateDatatypesAction.ISA):
+        if action in (Action.NODE1_TYPE,
+                      Action.NODE2_TYPE,
+                      Action.ISA):
             value = node2_value.value
                         
 
         return cls(node1_value, label_value, node2_value, action, pattern, intval, number, column_names, value)
 
 @attr.s(slots=True, frozen=True)
-class ValidateDatatypes:
+class PropertyPatternValidator:
     pass
