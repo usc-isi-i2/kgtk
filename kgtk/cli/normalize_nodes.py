9f4a1c335943a5d1aafe1434a7b45470234a3265
Craig Milo Rogers <rogers@isi.edu>
2020-07-21 12:10:29 -0700

    Optionally specify the ID column.  Optionally supply the label values.

--- kgtk/cli/normalize_nodes.py
+++ kgtk/cli/normalize_nodes.py
@@ -35,6 +35,12 @@ def add_arguments_extended(parser: KGTKArgumentParser, parsed_shared_args: Names
     parser.add_argument('-c', "--columns", action="store", type=str, dest="columns", nargs='+',
                         help="Columns to remove as a space-separated list. (default=all columns except id)")
 
+    parser.add_argument(      "--labels", action="store", type=str, dest="labels", nargs='+',
+                        help="Label names to use as a space-separated list. (default=column names)")
+
+    parser.add_argument(      "--id-column", action="store", type=str, dest="id_column_name",
+                        help="The name of the ID column. (default=id or alias)")
+
     KgtkReader.add_debug_arguments(parser, expert=_expert)
     KgtkReaderOptions.add_arguments(parser, mode_options=True, default_mode=KgtkReaderMode.NODE, expert=_expert)
     KgtkValueOptions.add_arguments(parser, expert=_expert)
@@ -43,6 +49,8 @@ def run(input_file: KGTKFiles,
         output_file: KGTKFiles,
 
         columns: typing.Optional[typing.List[str]] = None,
+        labels: typing.Optional[typing.List[str]] = None,
+        id_column_name: typing.Optional[str] = None,
 
         errors_to_stdout: bool = False,
         errors_to_stderr: bool = True,
@@ -72,10 +80,25 @@ def run(input_file: KGTKFiles,
 
         if columns is not None:
             print("--columns=%s" % " ".join(columns), file=error_file)
+        if labels is not None:
+            print("--labels=%s" % " ".join(labels), file=error_file)
+        if id_column_name is not None:
+            print("--id-column=%s" % id_column_name, file=error_file)
 
         reader_options.show(out=error_file)
         value_options.show(out=error_file)
         print("=======", file=error_file, flush=True)
+
+    label_map: typing.MutableMapping[str, str] = dict()
+    if labels is not None and len(labels) > 0:
+        if columns is None:
+            raise KGTKException("--columns must be supplied when --labels is used.")
+        if len(columns) != len(labels):
+            raise KGTKException("%d columns were supplied, but %d labels." % (len(columns), len(labels)))
+        idx: int
+        label: str
+        for idx, label in enumerate(labels):
+            label_map[columns[idx]] = label
 
     try:
         if verbose:
@@ -88,6 +111,10 @@ def run(input_file: KGTKFiles,
                                          very_verbose=very_verbose,
         )
 
+        id_column_idx: int = kr.get_id_column_index(id_column_name)
+        if id_column_idx < 0:
+            raise KGTKException("Unknown ID column %s" % repr(id_column_name))
+
         output_column_names: typing.List[str] = [ KgtkFormat.NODE1, KgtkFormat.LABEL, KgtkFormat.NODE2 ]
 
         if verbose:
@@ -98,29 +125,29 @@ def run(input_file: KGTKFiles,
                                          verbose=verbose,
                                          very_verbose=very_verbose)
 
-        shuffle_list: typing.List[int] = kw.build_shuffle_list(kr.column_names)
-
         input_line_count: int = 0
         output_line_count: int = 0
         row: typing.List[str]
         for row in kr:
             input_line_count += 1
 
-            node1_value: str = row[kr.id_column_idx]
+            node1_value: str = row[id_column_idx]
 
             column_idx: int
             column_name: str
             for column_idx, column_name in enumerate(kr.column_names):
-                if column_idx == kr.id_column_idx:
+                if column_idx == id_column_idx:
                     continue
                 if columns is not None and column_name not in columns:
                     continue
+
+                label_value: str = label_map.get(column_name, column_name)
 
                 node2_value: str = row[column_idx]
                 if len(node2_value) == 0:
                     continue
 
-                output_row: typing.List[str] = [ node1_value , column_name, node2_value ]
+                output_row: typing.List[str] = [ node1_value , label_value, node2_value ]
             
                 kw.write(output_row)
                 output_line_count += 1
