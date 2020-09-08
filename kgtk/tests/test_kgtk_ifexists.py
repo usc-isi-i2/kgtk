75b38d818ec348ba2f7f9aa62dfb57e0f5cba8ff
saggu <amandeep.s.saggu@gmail.com>
2020-09-08 10:27:57 -0700

    add kgtk ifexists test mode none

--- kgtk/tests/test_kgtk_ifexists.py
+++ kgtk/tests/test_kgtk_ifexists.py
@@ -8,7 +8,6 @@ from kgtk.cli_entry import cli_entry
 class TestKGTKFilter(unittest.TestCase):
     def setUp(self) -> None:
         self.file_path = 'data/sample_kgtk_edge_file.tsv'
-        self.Q47158_path = 'data/sample_kgtk_edge_Q47158.tsv'
         self.temp_dir = tempfile.mkdtemp()
         self.df = pd.read_csv(self.file_path, sep='\t')
 
@@ -16,8 +15,18 @@ class TestKGTKFilter(unittest.TestCase):
         shutil.rmtree(self.temp_dir)
 
     def test_kgtk_ifexists(self):
-        cli_entry("kgtk", "ifexists", "-i", self.file_path, "--filter-on", self.Q47158_path, "-o",
+        Q47158_path = 'data/sample_kgtk_edge_Q47158.tsv'
+        cli_entry("kgtk", "ifexists", "-i", self.file_path, "--filter-on", Q47158_path, "-o",
                   f'{self.temp_dir}/Q47158.tsv', "--input-keys", "node1", "--filter-keys", "node1", "--show-option")
+
+        df = pd.read_csv(f'{self.temp_dir}/Q47158.tsv', sep='\t')
+
+        self.assertEqual(len(df), 118)
+
+    def test_kgtk_ifexists_mode_none(self):
+        Q47158_path = 'data/Q47158_non_edge.tsv'
+        cli_entry("kgtk", "ifexists", "-i", self.file_path, "--filter-on", Q47158_path, "-o",
+                  f'{self.temp_dir}/Q47158.tsv', "--input-keys", "node1", "--filter-keys", "heading", "--mode", "NONE")
 
         df = pd.read_csv(f'{self.temp_dir}/Q47158.tsv', sep='\t')
 
