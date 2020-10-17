9cc76434b4621851c2c89956fd538a6c1b435f19
saggu <amandeep.s.saggu@gmail.com>
2020-10-16 17:05:05 -0700

    add graph cache for kgtk query tests

--- kgtk/tests/test_kgtk_query.py
+++ kgtk/tests/test_kgtk_query.py
@@ -14,29 +14,32 @@ class TestKGTKQuery(unittest.TestCase):
         self.works_path = 'data/kypher/works.tsv'
         self.props_path = 'data/kypher/props.tsv'
         self.temp_dir = tempfile.mkdtemp()
+        self.sqldb = f'{self.temp_dir}/test.sqlite3.db'
         self.df = pd.read_csv(self.file_path, sep='\t')
 
     def tearDown(self) -> None:
         shutil.rmtree(self.temp_dir)
 
     def test_kgtk_query_default(self):
-        cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv')
+        cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 9)
 
     def test_kgtk_query_default_gzip(self):
-        cli_entry("kgtk", "query", "-i", self.file_path_gz, "-o", f'{self.temp_dir}/out.tsv.gz')
+        cli_entry("kgtk", "query", "-i", self.file_path_gz, "-o", f'{self.temp_dir}/out.tsv.gz', '--graph-cache',
+                  self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv.gz', sep='\t')
         self.assertTrue(len(df) == 9)
 
     def test_kgtk_query_default_bz2(self):
-        cli_entry("kgtk", "query", "-i", self.file_path_bz2, "-o", f'{self.temp_dir}/out.tsv.bz2')
+        cli_entry("kgtk", "query", "-i", self.file_path_bz2, "-o", f'{self.temp_dir}/out.tsv.bz2', '--graph-cache',
+                  self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv.bz2', sep='\t')
         self.assertTrue(len(df) == 9)
 
     def test_kgtk_query_match(self):
         cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', "--match",
-                  "(i)-[:loves]->(c)")
+                  "(i)-[:loves]->(c)", '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 3)
         ids = list(df['id'].unique())
@@ -46,7 +49,7 @@ class TestKGTKQuery(unittest.TestCase):
 
     def test_kgtk_query_limit(self):
         cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', "--limit",
-                  "3")
+                  "3", '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 3)
         ids = list(df['id'].unique())
@@ -56,7 +59,7 @@ class TestKGTKQuery(unittest.TestCase):
 
     def test_kgtk_query_limit_skip(self):
         cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', "--limit",
-                  "3", "--skip", "2")
+                  "3", "--skip", "2", '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 3)
         ids = list(df['id'].unique())
@@ -66,7 +69,7 @@ class TestKGTKQuery(unittest.TestCase):
 
     def test_kgtk_query_hans_filter(self):
         cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', "--match",
-                  "(:Hans)-[]->()")
+                  "(:Hans)-[]->()", '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 2)
         ids = list(df['id'].unique())
@@ -75,7 +78,7 @@ class TestKGTKQuery(unittest.TestCase):
 
     def test_kgtk_query_otto_name_filter(self):
         cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', "--match",
-                  "(:Otto)-[:name]->()")
+                  "(:Otto)-[:name]->()", '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 1)
         ids = list(df['id'].unique())
@@ -83,7 +86,7 @@ class TestKGTKQuery(unittest.TestCase):
 
     def test_kgtk_query_where_double_letter(self):
         cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', "--match",
-                  "(p)-[:name]->(n)", "--where", 'n =~".*(.)\\\\1.*"')
+                  "(p)-[:name]->(n)", "--where", 'n =~".*(.)\\\\1.*"', '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 2)
         ids = list(df['id'].unique())
@@ -92,7 +95,7 @@ class TestKGTKQuery(unittest.TestCase):
 
     def test_kgtk_query_where_IN(self):
         cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', "--match",
-                  "(p)-[:name]->(n)", "--where", 'p IN ["Hans", "Susi"]')
+                  "(p)-[:name]->(n)", "--where", 'p IN ["Hans", "Susi"]', '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 2)
         ids = list(df['id'].unique())
@@ -101,7 +104,7 @@ class TestKGTKQuery(unittest.TestCase):
 
     def test_kgtk_query_where_upper_substring(self):
         cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', "--match",
-                  "(p)-[:name]->(n)", "--where", "upper(substr(n,2,1)) >= 'J'")
+                  "(p)-[:name]->(n)", "--where", "upper(substr(n,2,1)) >= 'J'", '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 4)
         ids = list(df['id'].unique())
@@ -112,7 +115,8 @@ class TestKGTKQuery(unittest.TestCase):
 
     def test_kgtk_query_where_upper_substring_sorted(self):
         cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', "--match",
-                  "(p)-[:name]->(n)", "--where", "upper(substr(n,2,1)) >= 'J'", "--order-by", "substr(n,2,1)")
+                  "(p)-[:name]->(n)", "--where", "upper(substr(n,2,1)) >= 'J'", "--order-by", "substr(n,2,1)",
+                  '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 4)
         ids = list(df['id'].unique())
@@ -123,7 +127,8 @@ class TestKGTKQuery(unittest.TestCase):
 
     def test_kgtk_query_where_upper_substring_sorted_desc(self):
         cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', "--match",
-                  "(p)-[:name]->(n)", "--where", "upper(substr(n,2,1)) >= 'J'", "--order-by", "substr(n,2,1) desc")
+                  "(p)-[:name]->(n)", "--where", "upper(substr(n,2,1)) >= 'J'", "--order-by", "substr(n,2,1) desc",
+                  '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 4)
         ids = list(df['id'].unique())
@@ -134,7 +139,8 @@ class TestKGTKQuery(unittest.TestCase):
 
     def test_kgtk_query_select_columns(self):
         cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', "--match",
-                  "(p)-[:name]->(n)", "--where", "upper(substr(n,2,1)) >= 'J'", "--return", "p,n")
+                  "(p)-[:name]->(n)", "--where", "upper(substr(n,2,1)) >= 'J'", "--return", "p,n", '--graph-cache',
+                  self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 4)
         columns = list(df.columns)
@@ -148,7 +154,8 @@ class TestKGTKQuery(unittest.TestCase):
 
     def test_kgtk_query_switch_columns(self):
         cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', "--match",
-                  "(p)-[r:name]->(n)", "--where", "upper(substr(n,2,1)) >= 'J'", "--return", "p,n, r, r.label")
+                  "(p)-[r:name]->(n)", "--where", "upper(substr(n,2,1)) >= 'J'", "--return", "p,n, r, r.label",
+                  '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 4)
         columns = list(df.columns)
@@ -164,7 +171,8 @@ class TestKGTKQuery(unittest.TestCase):
 
     def test_kgtk_query_return_columns_modify_functions(self):
         cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', "--match",
-                  "(p)-[r:name]->(n)", "--where", "upper(substr(n,2,1)) >= 'J'", "--return", "lower(p), r.label, n, r")
+                  "(p)-[r:name]->(n)", "--where", "upper(substr(n,2,1)) >= 'J'", "--return", "lower(p), r.label, n, r",
+                  '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 4)
         columns = list(df.columns)
@@ -181,7 +189,7 @@ class TestKGTKQuery(unittest.TestCase):
     def test_kgtk_query_kgtk_unstringify(self):
         cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', "--match",
                   "(p)-[r:name]->(n)", "--where", "upper(substr(n,2,1)) >= 'J'", "--return",
-                  "p, r.label, kgtk_unstringify(n), r")
+                  "p, r.label, kgtk_unstringify(n), r", '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 4)
         columns = list(df.columns)
@@ -195,7 +203,8 @@ class TestKGTKQuery(unittest.TestCase):
     def test_kgtk_query_para(self):
         cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', "--match",
                   "(p)-[r:name]->(n)", "--where", " n = $name OR n = $name2 OR n = $name3 ",
-                  "--para", "name=\"'Hans'@de\"", "--spara", "name2=Susi", "--lqpara", "name3=Otto@de")
+                  "--para", "name=\"'Hans'@de\"", "--spara", "name2=Susi", "--lqpara", "name3=Otto@de", '--graph-cache',
+                  self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 2)
         columns = list(df.columns)
@@ -209,7 +218,7 @@ class TestKGTKQuery(unittest.TestCase):
 
     def test_kgtk_query_lgstring_land(self):
         cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', "--match",
-                  "(p)-[r:name]->(n)", "--where", 'n.kgtk_lqstring_lang = "de"')
+                  "(p)-[r:name]->(n)", "--where", 'n.kgtk_lqstring_lang = "de"', '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 2)
         ids = list(df['id'].unique())
@@ -218,7 +227,7 @@ class TestKGTKQuery(unittest.TestCase):
 
     def test_kgtk_query_reflexive_edges(self):
         cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', "--match",
-                  "(a)-[]->(a)")
+                  "(a)-[]->(a)", '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 1)
         ids = list(df['id'].unique())
@@ -226,7 +235,8 @@ class TestKGTKQuery(unittest.TestCase):
 
     def test_kgtk_query_multi_step_path(self):
         cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', "--match",
-                  "(na)<-[:name]-(a)-[r:loves]->(b)-[:name]->(nb)", "--return", "r, na, r.label, nb")
+                  "(na)<-[:name]-(a)-[r:loves]->(b)-[:name]->(nb)", "--return", "r, na, r.label, nb", '--graph-cache',
+                  self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 3)
         ids = list(df['id'].unique())
@@ -246,7 +256,7 @@ class TestKGTKQuery(unittest.TestCase):
         cli_entry("kgtk", "query", "-i", self.file_path, "-o", f'{self.temp_dir}/out.tsv', "--match",
                   "(na)<-[:name]-(a)-[r:loves]->(b)-[:name]->(nb)",
                   "--where", 'na.kgtk_lqstring_lang = "de" OR nb.kgtk_lqstring_lang = "de"',
-                  "--return", "r, na, r.label, nb")
+                  "--return", "r, na, r.label, nb", '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 2)
         ids = list(df['id'].unique())
@@ -263,7 +273,7 @@ class TestKGTKQuery(unittest.TestCase):
         cli_entry("kgtk", "query", "-i", self.file_path,
                   "-i", self.works_path,
                   "-o", f'{self.temp_dir}/out.tsv', "--match",
-                  "g: (x)-[:loves]->(y), w: (y)-[:works]-(c)")
+                  "g: (x)-[:loves]->(y), w: (y)-[:works]-(c)", '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 3)
         for i, row in df.iterrows():
@@ -281,7 +291,7 @@ class TestKGTKQuery(unittest.TestCase):
         cli_entry("kgtk", "query", "-i", self.file_path,
                   "-i", self.works_path,
                   "-o", f'{self.temp_dir}/out.tsv', "--match",
-                  "(x)-[:loves]->(y), w: (y)-[:works]-(c)")
+                  "(x)-[:loves]->(y), w: (y)-[:works]-(c)", '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 3)
         for i, row in df.iterrows():
@@ -300,7 +310,7 @@ class TestKGTKQuery(unittest.TestCase):
                   "-i", self.works_path,
                   "-o", f'{self.temp_dir}/out.tsv', "--match",
                   "g: (x)-[r:loves]->(y), w: (y)-[:works]-(c)",
-                  "--return", 'r, x, r.label, y as node2, c as `node2;work`')
+                  "--return", 'r, x, r.label, y as node2, c as `node2;work`', '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 3)
         for i, row in df.iterrows():
@@ -315,7 +325,8 @@ class TestKGTKQuery(unittest.TestCase):
                   "-o", f'{self.temp_dir}/out.tsv', "--match",
                   "g: (x)-[r:loves]->(y), w: (y {salary: s})-[:works]-(c)",
                   "--where", "cast(s, integer) >= 10000",
-                  "--return", 'r, x, r.label, y as node2, c as `node2;work`, s as `node2;salary`')
+                  "--return", 'r, x, r.label, y as node2, c as `node2;work`, s as `node2;salary`', '--graph-cache',
+                  self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
 
         self.assertTrue(len(df) == 2)
@@ -329,7 +340,7 @@ class TestKGTKQuery(unittest.TestCase):
         cli_entry("kgtk", "query", "-i", self.file_path,
                   "-o", f'{self.temp_dir}/out.tsv', "--match",
                   "g: (x)-[r]->(y)",
-                  "--return", 'max(x) as node1, r.label, y, r')
+                  "--return", 'max(x) as node1, r.label, y, r', '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 1)
         for i, row in df.iterrows():
@@ -342,7 +353,7 @@ class TestKGTKQuery(unittest.TestCase):
                   "-o", f'{self.temp_dir}/out.tsv', "--match",
                   "g: (x)-[r]->(y)",
                   "--return", 'r, max(x), r.label, y',
-                  "--limit", "5")
+                  "--limit", "5", '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
 
         self.assertTrue(len(df) == 5)
@@ -358,7 +369,7 @@ class TestKGTKQuery(unittest.TestCase):
                   "-o", f'{self.temp_dir}/out.tsv', "--match",
                   "g: (x)-[r]->(y)",
                   "--where", 'x = "Joe"',
-                  "--return", 'count(x) as N')
+                  "--return", 'count(x) as N', '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
 
         self.assertTrue(len(df) == 1)
@@ -370,7 +381,7 @@ class TestKGTKQuery(unittest.TestCase):
                   "-o", f'{self.temp_dir}/out.tsv', "--match",
                   "g: (x)-[r]->(y)",
                   "--where", 'x = "Joe"',
-                  "--return", 'count(distinct x) as N')
+                  "--return", 'count(distinct x) as N', '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 1)
         for i, row in df.iterrows():
@@ -380,7 +391,8 @@ class TestKGTKQuery(unittest.TestCase):
         cli_entry("kgtk", "query", "-i", self.works_path,
                   "-o", f'{self.temp_dir}/out.tsv', "--match",
                   "w: (y {salary: s})-[r:works]-(c)",
-                  "--return", 'max(cast(s, int)) as `node1;salary`, y, "works" as label, c, r')
+                  "--return", 'max(cast(s, int)) as `node1;salary`, y, "works" as label, c, r', '--graph-cache',
+                  self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 1)
         for i, row in df.iterrows():
@@ -390,7 +402,7 @@ class TestKGTKQuery(unittest.TestCase):
 
     def test_kgtk_query_date_filter(self):
         cli_entry("kgtk", "query", "-i", self.quals_path, "-o", f'{self.temp_dir}/out.tsv', "--match",
-                  "(eid)-[q]->(time)", "--where", "time.kgtk_date_year < 2005")
+                  "(eid)-[q]->(time)", "--where", "time.kgtk_date_year < 2005", '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 4)
         ids = list(df['id'].unique())
@@ -406,7 +418,7 @@ class TestKGTKQuery(unittest.TestCase):
                   "-o", f'{self.temp_dir}/out.tsv', "--match",
                   "work: (x)-[r {label: rl}]->(y), qual: (r)-[rp {label: p}]->(time), prop: (p)-[:member]->(:set1)",
                   "--where", 'time.kgtk_date_year <= 2000',
-                  "--return", 'r as id, x, rl, y, p as trel, time as time')
+                  "--return", 'r as id, x, rl, y, p as trel, time as time', '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 3)
         for i, row in df.iterrows():
@@ -422,7 +434,7 @@ class TestKGTKQuery(unittest.TestCase):
                   "-o", f'{self.temp_dir}/out.tsv', "--match",
                   "work: (x)-[r {label: rl}]->(y), qual: (r)-[rp {label: p}]->(time)",
                   "--where", "p in ['starts', 'ends'] and time.kgtk_date_year <= 2000",
-                  "--return", 'r as id, x, rl, y, p as trel, time as time')
+                  "--return", 'r as id, x, rl, y, p as trel, time as time', '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 3)
         for i, row in df.iterrows():
@@ -445,7 +457,7 @@ class TestKGTKQuery(unittest.TestCase):
                   "work: (x)-[r {label: rl}]->(y), qual: (r)-[rp {label: p}]->(time)",
                   "--where", "p =~ 's.*' and time.kgtk_date_year <= 2000",
                   "--return", 'r as id, x, rl, y, p as trel, time as time',
-                  "--order-by", 'p desc, time asc')
+                  "--order-by", 'p desc, time asc', '--graph-cache', self.sqldb)
         df = pd.read_csv(f'{self.temp_dir}/out.tsv', sep='\t')
         self.assertTrue(len(df) == 2)
         for i, row in df.iterrows():
