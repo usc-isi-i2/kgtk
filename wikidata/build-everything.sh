dcc5a541873b3ae5c052cf476666347ed7f14d7a
Craig Milo Rogers <rogers@isi.edu>
2020-11-04 18:42:02 -0800

    Fix typos.

--- wikidata/build-everything.sh
+++ wikidata/build-everything.sh
@@ -9,9 +9,9 @@ kgtk ${KGTK_FLAGS} \
      -i /data4/rogers/elicit/cache/datasets/wikidata-20200803/data/part.alias.tsv.gz \
      -i /data4/rogers/elicit/cache/datasets/wikidata-20200803/data/part.description.tsv.gz \
      -i /data4/rogers/elicit/cache/datasets/wikidata-20200803/data/part.label.tsv.gz \
-     -i /data4/rogers/elicit/cache/datasets/wikidata-20200803/data/part.wikoipedia_sitelink.tsv.gz \
+     -i /data4/rogers/elicit/cache/datasets/wikidata-20200803/data/part.wikipedia_sitelink.tsv.gz \
      -i /data4/rogers/elicit/cache/datasets/wikidata-20200803/data/types.tsv.gz \
      -i /data4/rogers/elicit/cache/datasets/wikidata-20200803/data/property.datatype.tsv.gz \
-     -o /data4/rogers/elicit/cache/datasets/wikidata-20200803/data/everythingtsv.gz \
+     -o /data4/rogers/elicit/cache/datasets/wikidata-20200803/data/everything.tsv.gz \
 
 
