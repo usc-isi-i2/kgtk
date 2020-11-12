1166ca7184fcb0fc898f639009c3b85b25832b11
Craig Milo Rogers <rogers@isi.edu>
2020-11-12 12:29:16 -0800

    Count entities and properties in various places.

--- wikidata/extract-properties.sh
+++ wikidata/extract-properties.sh
@@ -2,7 +2,7 @@
 
 source common.sh
 
-echo -e "\nExtract claims with a property in node1."
+echo -e "\nExtract claims with a property in the node1 column."
 kgtk $KGTK_FLAGS filter $VERBOSE --use-mgzip=$USE_MGZIP --regex\
      --input-file $DATADIR/claims.$SORTED_KGTK \
      -p '^P.*$ ;;' -o $DATADIR/claims.properties.$SORTED_KGTK
@@ -11,3 +11,4 @@ echo -e "\nExtract the qualifiers for claims with a property in node1."
 kgtk $KGTK_FLAGS filter $VERBOSE --use-mgzip=$USE_MGZIP --regex \
      --input-file $DATADIR/qualifiers.$SORTED_KGTK \
     -p '^P.*$ ;;' -o $DATADIR/qualifiers.properties.$SORTED_KGTK
+
