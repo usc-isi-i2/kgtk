09122e3663a6673cff92cc9e189def91e8e422f1
Craig Milo Rogers <rogers@isi.edu>
2020-11-12 13:39:33 -0800

    Filter entities and lift labels.

--- wikidata/count-adls-entities.sh
+++ wikidata/count-adls-entities.sh
@@ -3,42 +3,66 @@
 source common.sh
 
 # ==============================================================================
-echo -e "\nCount the entities in the node1 column in ${DATADIR}/aliases.${SORTED_KGTK}."
+echo -e "\nCount the entities in the node1 column in ${DATADIR}/aliases.${SORTED_KGTK} and lift labels."
 kgtk ${KGTK_FLAGS} \
      unique ${VERBOSE} \
      --input-file ${DATADIR}/aliases.${SORTED_KGTK} \
-     --output-file ${DATADIR}/aliases.node1.entity.counts.${SORTED_KGTK} \
      --column node1 \
      --label node1-entity-count \
+     --use-mgzip ${USE_MGZIP} \
+     / lift ${VERBOSE} \
+     --label-file ${DATADIR}/labels.en.${SORTED_KGTK} \
+     --output-file ${DATADIR}/aliases.node1.entity.counts.${SORTED_KGTK} \
+     --columns-to-lift node1 \
+     --input-file-is-presorted \
+     --label-file-is-presorted \
      --use-mgzip ${USE_MGZIP} \
      |& tee ${LOGDIR}/aliases.node1.entity.counts.log
 
-echo -e "\nCount the entities in the node1 column in ${DATADIR}/descriptions.${SORTED_KGTK}."
+echo -e "\nCount the entities in the node1 column in ${DATADIR}/descriptions.${SORTED_KGTK} and lift labels."
 kgtk ${KGTK_FLAGS} \
      unique ${VERBOSE} \
      --input-file ${DATADIR}/descriptions.${SORTED_KGTK} \
-     --output-file ${DATADIR}/descriptions.node1.entity.counts.${SORTED_KGTK} \
      --column node1 \
      --label node1-entity-count \
+     --use-mgzip ${USE_MGZIP} \
+     / lift ${VERBOSE} \
+     --label-file ${DATADIR}/labels.en.${SORTED_KGTK} \
+     --output-file ${DATADIR}/descriptions.node1.entity.counts.${SORTED_KGTK} \
+     --columns-to-lift node1 \
+     --input-file-is-presorted \
+     --label-file-is-presorted \
      --use-mgzip ${USE_MGZIP} \
      |& tee ${LOGDIR}/descriptions.node1.entity.counts.log
 
-echo -e "\nCount the entities in the node1 column in ${DATADIR}/labels.${SORTED_KGTK}."
+echo -e "\nCount the entities in the node1 column in ${DATADIR}/labels.${SORTED_KGTK} and lift labels."
 kgtk ${KGTK_FLAGS} \
      unique ${VERBOSE} \
      --input-file ${DATADIR}/labels.${SORTED_KGTK} \
-     --output-file ${DATADIR}/labels.node1.entity.counts.${SORTED_KGTK} \
      --column node1 \
      --label node1-entity-count \
      --use-mgzip ${USE_MGZIP} \
+     / lift ${VERBOSE} \
+     --label-file ${DATADIR}/labels.en.${SORTED_KGTK} \
+     --output-file ${DATADIR}/labels.node1.entity.counts.${SORTED_KGTK} \
+     --columns-to-lift node1 \
+     --input-file-is-presorted \
+     --label-file-is-presorted \
+     --use-mgzip ${USE_MGZIP} \
      |& tee ${LOGDIR}/labels.node1.entity.counts.log
 
-echo -e "\nCount the entities in the node1 column in ${DATADIR}/sitelinks.${SORTED_KGTK}."
+echo -e "\nCount the entities in the node1 column in ${DATADIR}/sitelinks.${SORTED_KGTK} and lift labels."
 kgtk ${KGTK_FLAGS} \
      unique ${VERBOSE} \
      --input-file ${DATADIR}/sitelinks.${SORTED_KGTK} \
-     --output-file ${DATADIR}/sitelinks.node1.entity.counts.${SORTED_KGTK} \
      --column node1 \
      --label node1-entity-count \
+     --use-mgzip ${USE_MGZIP} \
+     / lift ${VERBOSE} \
+     --label-file ${DATADIR}/labels.en.${SORTED_KGTK} \
+     --output-file ${DATADIR}/sitelinks.node1.entity.counts.${SORTED_KGTK} \
+     --columns-to-lift node1 \
+     --input-file-is-presorted \
+     --label-file-is-presorted \
      --use-mgzip ${USE_MGZIP} \
      |& tee ${LOGDIR}/sitelinks.node1.entity.counts.log
--- wikidata/count-claims-properties.sh
+++ wikidata/count-claims-properties.sh
@@ -2,39 +2,57 @@
 
 source common.sh
 
-echo -e "\nCount the properties in the node1 column in ${DATADIR}/claims.${SORTED_KGTK}."
+echo -e "\nCount the properties in the node1 column in ${DATADIR}/claims.${SORTED_KGTK} and lift English labels."
 kgtk ${KGTK_FLAGS} \
      filter $VERBOSE --use-mgzip=$USE_MGZIP --regex \
      --input-file $DATADIR/claims.$SORTED_KGTK \
      -p '^P.*$ ;;' -o - \
-     unique ${VERBOSE} \
-     --output-file ${DATADIR}/claims.node1.property.counts.${SORTED_KGTK} \
+     / unique ${VERBOSE} \
      --column node1 \
      --label node1-property-count \
      --use-mgzip ${USE_MGZIP} \
+     / lift ${VERBOSE} \
+     --label-file ${DATADIR}/labels.en.${SORTED_KGTK} \
+     --output-file ${DATADIR}/claims.node1.property.counts.${SORTED_KGTK} \
+     --columns-to-lift node1 \
+     --input-file-is-presorted \
+     --label-file-is-presorted \
+     --use-mgzip ${USE_MGZIP} \
      |& tee ${LOGDIR}/claims.node1.property.counts.log
 
-echo -e "\nCount the properties in the label column in ${DATADIR}/claims.${SORTED_KGTK}."
+echo -e "\nCount the properties in the label column in ${DATADIR}/claims.${SORTED_KGTK} and lift English labels."
 kgtk ${KGTK_FLAGS} \
      filter $VERBOSE --use-mgzip=$USE_MGZIP --regex \
      --input-file $DATADIR/claims.$SORTED_KGTK \
      -p '; ^P.*$ ;' -o - \
-     unique ${VERBOSE} \
-     --output-file ${DATADIR}/claims.node1.property.counts.${SORTED_KGTK} \
+     / unique ${VERBOSE} \
      --column label \
      --label label-property-count \
      --use-mgzip ${USE_MGZIP} \
+     / lift ${VERBOSE} \
+     --label-file ${DATADIR}/labels.en.${SORTED_KGTK} \
+     --output-file ${DATADIR}/claims.node1.property.counts.${SORTED_KGTK} \
+     --columns-to-lift node1 \
+     --input-file-is-presorted \
+     --label-file-is-presorted \
+     --use-mgzip ${USE_MGZIP} \
      |& tee ${LOGDIR}/claims.label.property.counts.log
 
-echo -e "\nCount the properties in the node2 column in ${DATADIR}/claims.${SORTED_KGTK}."
+echo -e "\nCount the properties in the node2 column in ${DATADIR}/claims.${SORTED_KGTK} and lift English labels."
 kgtk ${KGTK_FLAGS} \
      filter $VERBOSE --use-mgzip=$USE_MGZIP --regex \
      --input-file $DATADIR/claims.$SORTED_KGTK \
      -p ';; ^P.*$' -o - \
-     unique ${VERBOSE} \
-     --output-file ${DATADIR}/claims.node2.property.counts.${SORTED_KGTK} \
+     / unique ${VERBOSE} \
      --column node2 \
      --label node2-property-count \
+     --use-mgzip ${USE_MGZIP} \
+     / lift ${VERBOSE} \
+     --label-file ${DATADIR}/labels.en.${SORTED_KGTK} \
+     --output-file ${DATADIR}/claims.node2.property.counts.${SORTED_KGTK} \
+     --columns-to-lift node1 \
+     --input-file-is-presorted \
+     --label-file-is-presorted \
      --use-mgzip ${USE_MGZIP} \
      |& tee ${LOGDIR}/claims.node2.property.counts.log
 
