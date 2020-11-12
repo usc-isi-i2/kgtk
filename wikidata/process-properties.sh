990aa2f4423dab91a5818bea002167d61beb58d1
Craig Milo Rogers <rogers@isi.edu>
2020-11-12 11:27:15 -0800

    Extract the property claims in a seperate file. Rename build-everything.

--- wikidata/process-properties.sh
+++ wikidata/process-properties.sh
@@ -2,17 +2,6 @@
 
 source common.sh
 
-kgtk $KGTK_FLAGS filter $VERBOSE --use-mgzip=$USE_MGZIP --regex\
-     --input-file $DATADIR/claims.$SORTED_KGTK \
-     -p '^P.*$ ;;' -o $DATADIR/claims.properties.$SORTED_KGTK
-
-kgtk $KGTK_FLAGS filter $VERBOSE --use-mgzip=$USE_MGZIP --regex \
-     --input-file $DATADIR/qualifiers.$SORTED_KGTK \
-    -p '^P.*$ ;;' -o $DATADIR/qualifiers.properties.$SORTED_KGTK
-
+./extract-properties.sh
 ./count-properties.sh
 ./count-property-datatypes.sh
-
-
-# ./split-properties-by-datatype.sh
-# ./extract-qualifiers-for-property-datatype-splits.sh
