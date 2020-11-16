3bb593d012394bd5c7d197c7516deaf0ab64d056
Craig Milo Rogers <rogers@isi.edu>
2020-11-16 11:04:24 -0800

    Count the qualifier entities and properties, and all properties.

--- wikidata/count-all-properties.sh
+++ wikidata/count-all-properties.sh
@@ -3,7 +3,7 @@
 source common.sh
 
 # ==============================================================================
-echo -e "\nCount the entities in the node1 column in ${DATADIR}/all.${SORTED_KGTK} and lift English labels."
+echo -e "\nCount the properties in the node1 column in ${DATADIR}/all.${SORTED_KGTK} and lift English labels."
 kgtk ${KGTK_FLAGS} \
      filter $VERBOSE --use-mgzip=$USE_MGZIP --regex \
      --input-file ${DATADIR}/all.${SORTED_KGTK} \
@@ -13,13 +13,13 @@ kgtk ${KGTK_FLAGS} \
      --label node1-entity-count \
      / lift ${VERBOSE} --use-mgzip=$USE_MGZIP \
      --label-file ${DATADIR}/labels.en.${SORTED_KGTK} \
-     --output-file ${DATADIR}/all.node1.entity.counts.${SORTED_KGTK} \
+     --output-file ${DATADIR}/all.node1.property.counts.${SORTED_KGTK} \
      --columns-to-lift node1 \
      --input-file-is-presorted \
      --label-file-is-presorted \
-     |& tee ${LOGDIR}/all.node1.entity.counts.log
+     |& tee ${LOGDIR}/all.node1.property.counts.log
 
-echo -e "\nCount the entities in the label column in ${DATADIR}/all.${SORTED_KGTK} and lift English labels."
+echo -e "\nCount the properties in the label column in ${DATADIR}/all.${SORTED_KGTK} and lift English labels."
 kgtk ${KGTK_FLAGS} \
      filter $VERBOSE --use-mgzip=$USE_MGZIP --regex \
      --input-file ${DATADIR}/all.${SORTED_KGTK} \
@@ -29,13 +29,13 @@ kgtk ${KGTK_FLAGS} \
      --label label-entity-count \
      / lift ${VERBOSE} --use-mgzip=$USE_MGZIP \
      --label-file ${DATADIR}/labels.en.${SORTED_KGTK} \
-     --output-file ${DATADIR}/all.label.entity.counts.${SORTED_KGTK} \
+     --output-file ${DATADIR}/all.label.property.counts.${SORTED_KGTK} \
      --columns-to-lift node1 \
      --input-file-is-presorted \
      --label-file-is-presorted \
-     |& tee ${LOGDIR}/all.label.entity.counts.log
+     |& tee ${LOGDIR}/all.label.property.counts.log
 
-echo -e "\nCount the entities in the node2 column in ${DATADIR}/all.${SORTED_KGTK} and lift English labels."
+echo -e "\nCount the properties in the node2 column in ${DATADIR}/all.${SORTED_KGTK} and lift English labels."
 kgtk ${KGTK_FLAGS} \
      filter $VERBOSE --use-mgzip=$USE_MGZIP --regex \
      --input-file ${DATADIR}/all.${SORTED_KGTK} \
@@ -45,8 +45,8 @@ kgtk ${KGTK_FLAGS} \
      --label node2-entity-count \
      / lift ${VERBOSE} --use-mgzip=$USE_MGZIP \
      --label-file ${DATADIR}/labels.en.${SORTED_KGTK} \
-     --output-file ${DATADIR}/all.node2.entity.counts.${SORTED_KGTK} \
+     --output-file ${DATADIR}/all.node2.property.counts.${SORTED_KGTK} \
      --columns-to-lift node1 \
      --input-file-is-presorted \
      --label-file-is-presorted \
-     |& tee ${LOGDIR}/all.node2.entity.counts.log
+     |& tee ${LOGDIR}/all.node2.propertycounts.log
