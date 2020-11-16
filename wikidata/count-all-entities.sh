90b29a143e991e862064b38fa7d2b1ece2194bd9
Craig Milo Rogers <rogers@isi.edu>
2020-11-15 17:09:04 -0800

    Reposition --use-mgzip and note when the input is presorted.

--- wikidata/count-all-entities.sh
+++ wikidata/count-all-entities.sh
@@ -8,17 +8,15 @@ kgtk ${KGTK_FLAGS} \
      filter $VERBOSE --use-mgzip=$USE_MGZIP --regex \
      --input-file ${DATADIR}/all.${SORTED_KGTK} \
      -p '^[PQ][^-]*$ ;;' -o - \
-     / unique ${VERBOSE} \
+     / unique ${VERBOSE} --use-mgzip=$USE_MGZIP --presorted \
      --column node1 \
      --label node1-entity-count \
-     --use-mgzip ${USE_MGZIP} \
-     / lift ${VERBOSE} \
+     / lift ${VERBOSE} --use-mgzip=$USE_MGZIP \
      --label-file ${DATADIR}/labels.en.${SORTED_KGTK} \
      --output-file ${DATADIR}/all.node1.entity.counts.${SORTED_KGTK} \
      --columns-to-lift node1 \
      --input-file-is-presorted \
      --label-file-is-presorted \
-     --use-mgzip ${USE_MGZIP} \
      |& tee ${LOGDIR}/all.node1.entity.counts.log
 
 echo -e "\nCount the entities in the label column in ${DATADIR}/all.${SORTED_KGTK} and lift English labels."
@@ -26,17 +24,15 @@ kgtk ${KGTK_FLAGS} \
      filter $VERBOSE --use-mgzip=$USE_MGZIP --regex \
      --input-file ${DATADIR}/all.${SORTED_KGTK} \
      -p '; ^[PQ][^-]*$ ;' -o - \
-     / unique ${VERBOSE} \
+     / unique ${VERBOSE} --use-mgzip=$USE_MGZIP \
      --column label \
      --label label-entity-count \
-     --use-mgzip ${USE_MGZIP} \
-     / lift ${VERBOSE} \
+     / lift ${VERBOSE} --use-mgzip=$USE_MGZIP \
      --label-file ${DATADIR}/labels.en.${SORTED_KGTK} \
      --output-file ${DATADIR}/all.label.entity.counts.${SORTED_KGTK} \
      --columns-to-lift node1 \
      --input-file-is-presorted \
      --label-file-is-presorted \
-     --use-mgzip ${USE_MGZIP} \
      |& tee ${LOGDIR}/all.label.entity.counts.log
 
 echo -e "\nCount the entities in the node2 column in ${DATADIR}/all.${SORTED_KGTK} and lift English labels."
@@ -44,15 +40,13 @@ kgtk ${KGTK_FLAGS} \
      filter $VERBOSE --use-mgzip=$USE_MGZIP --regex \
      --input-file ${DATADIR}/all.${SORTED_KGTK} \
      -p ';; ^[PQ][^-]*$' -o - \
-     / unique ${VERBOSE} \
+     / unique ${VERBOSE} --use-mgzip=$USE_MGZIP \
      --column node2 \
      --label node2-entity-count \
-     --use-mgzip ${USE_MGZIP} \
-     / lift ${VERBOSE} \
+     / lift ${VERBOSE} --use-mgzip=$USE_MGZIP \
      --label-file ${DATADIR}/labels.en.${SORTED_KGTK} \
      --output-file ${DATADIR}/all.node2.entity.counts.${SORTED_KGTK} \
      --columns-to-lift node1 \
      --input-file-is-presorted \
      --label-file-is-presorted \
-     --use-mgzip ${USE_MGZIP} \
      |& tee ${LOGDIR}/all.node2.entity.counts.log
