7e8c55098f295ce9c30160aaa227cc3f0bfe65e5
Craig Milo Rogers <rogers@isi.edu>
2020-11-18 13:34:01 -0800

    Use 0 as a default value when counting entities/properties.  Run the commands as a pipe. Add labels at the end.

--- wikidata/total-unclaimed-property-counts.sh
+++ wikidata/total-unclaimed-property-counts.sh
@@ -11,26 +11,38 @@ kgtk ${KGTK_FLAGS} \
      --input-file ${DATADIR}/all.node2.property.counts.unclaimed.${SORTED_KGTK} \
      / unique $VERBOSE --use-mgzip=$USE_MGZIP \
      --column node1 \
-     --label slot-count \
      / lift $VERBOSE --use-mgzip=$USE_MGZIP \
      --label-file ${DATADIR}/all.node1.property.counts.unclaimed.${SORTED_KGTK} \
      --columns-to-lift node1 \
      --property node1-property-count \
+     --columns-to-write 'node1;node1-property-count' \
+     --default-value 0 \
      --input-file-is-presorted \
      --label-file-is-presorted \
      / lift $VERBOSE --use-mgzip=$USE_MGZIP \
      --label-file ${DATADIR}/all.label.property.counts.unclaimed.${SORTED_KGTK} \
      --columns-to-lift node1 \
      --property label-property-count \
+     --columns-to-write 'node1;label-property-count' \
+     --default-value 0 \
      --input-file-is-presorted \
      --label-file-is-presorted \
      / lift $VERBOSE --use-mgzip=$USE_MGZIP \
      --label-file ${DATADIR}/all.node2.property.counts.unclaimed.${SORTED_KGTK} \
      --columns-to-lift node1 \
      --property node2-property-count \
+     --columns-to-write 'node1;node2-property-count' \
+     --default-value 0 \
      --input-file-is-presorted \
      --label-file-is-presorted \
      / calc $VERBOSE --use-mgzip=$USE_MGZIP \
      --columns 'node1;node1-property-count' 'node1;label-property-count' 'node1;node2-property-count' \
-     --do sum --into 'node1;total-property-count' \
+     --do sum --into 'node1;total-property-count' --format '%d' \
+     / lift $VERBOSE --use-mgzip=$USE_MGZIP \
+     --label-file ${DATADIR}/labels.en.${SORTED_KGTK} \
+     --columns-to-lift node1 \
+     --input-file-is-presorted \
+     --label-file-is-presorted \
      --output-file ${DATADIR}/all.total.property.counts.unclaimed.${SORTED_KGTK}
+
+
