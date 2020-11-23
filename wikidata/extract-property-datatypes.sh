d6d2297463816632e7d0f5fdc7efa2ff8cc59e3f
Craig Milo Rogers <rogers@isi.edu>
2020-11-23 09:59:45 -0800

    Perform the analysis in multiple steps.  The piplined approach was consuming too many resources.

--- wikidata/extract-property-datatypes.sh
+++ wikidata/extract-property-datatypes.sh
@@ -13,7 +13,11 @@ kgtk ${KGTK_FLAGS} \
      filter $VERBOSE --use-mgzip=$USE_MGZIP --regex \
      --input-file ${DATADIR}/${PART}.${SORTED_KGTK} \
      -p '; ^P[^-]*$ ;' \
-     / calc ${VERBOSE} --use-mgzip=$USE_MGZIP \
+     --output-file ${DATADIR}/${PART}.properties.${SORTED_KGTK}
+
+kgtk ${KGTK_FLAGS} \
+     calc ${VERBOSE} --use-mgzip=$USE_MGZIP \
+     --input-file ${DATADIR}/${PART}.properties.${SORTED_KGTK} \
      --do copy \
      --columns  label 'node2;wikidatatype' \
      --into     node1  node2 \
@@ -21,25 +25,50 @@ kgtk ${KGTK_FLAGS} \
      --do set --value datatype --into label \
      / remove-columns ${VERBOSE} --use-mgzip=$USE_MGZIP --split-on-spaces \
      --all-except --columns node1 label node2 \
-     / sort2 ${VERBOSE} --use-mgzip=$USE_MGZIP \
+     --output-file ${DATADIR}/${PART}.properties.datatypes.unsorted.${SORTED_KGTK}
+     
+kgtk ${KGTK_FLAGS} \
+     sort2 ${VERBOSE} --use-mgzip=$USE_MGZIP \
+     --input-file ${DATADIR}/${PART}.properties.datatypes.unsorted.${SORTED_KGTK} \
      --gzip-command ${GZIP_CMD} \
      --extra "${SORT_EXTRAS}" \
-     / compact ${VERBOSE} --use-mgzip=$USE_MGZIP --presorted \
-     / lift ${VERBOSE} --use-mgzip=${USE_MGZIP} \
+     --output-file ${DATADIR}/${PART}.properties.datatypes.sorted.${SORTED_KGTK}
+
+kgtk ${KGTK_FLAGS} \
+     compact ${VERBOSE} --use-mgzip=$USE_MGZIP --presorted \
+     --input-file ${DATADIR}/${PART}.properties.datatypes.sorted.${SORTED_KGTK} \
+     --output-file ${DATADIR}/${PART}.properties.datatypes.compacted.${SORTED_KGTK}
+
+kgtk ${KGTK_FLAGS} \
+     lift ${VERBOSE} --use-mgzip=${USE_MGZIP} \
+     --input-file ${DATADIR}/${PART}.properties.datatypes.compacted.${SORTED_KGTK} \
      --columns-to-lift node1 \
      --input-file-is-presorted \
      --label-file ${DATADIR}/labels.en.${SORTED_KGTK} \
      --label-file-is-presorted \
-     / tee $VERBOSE --use-mgzip=$USE_MGZIP \
-     --into-file ${DATADIR}/${PART}.properties.datatypes.${SORTED_KGTK} \
-     / unique ${VERBOSE} --use-mgzip=$USE_MGZIP --presorted \
+     --output-file ${DATADIR}/${PART}.properties.datatypes.${SORTED_KGTK} \
+
+#     / tee $VERBOSE --use-mgzip=$USE_MGZIP \
+#     --into-file ${DATADIR}/${PART}.properties.datatypes.${SORTED_KGTK} \
+
+kgtk ${KGTK_FLAGS} \
+     unique ${VERBOSE} --use-mgzip=$USE_MGZIP --presorted \
+     --input-file ${DATADIR}/${PART}.properties.datatypes.${SORTED_KGTK} \
      --column node1 \
      --label datatype-count \
-     / lift ${VERBOSE} --use-mgzip=${USE_MGZIP} \
+     --output-file ${DATADIR}/${PART}.properties.datatypes.counts.unsorted.${SORTED_KGTK} \
+
+kgtk ${KGTK_FLAGS} \
+     lift ${VERBOSE} --use-mgzip=${USE_MGZIP} \
+     --input-file ${DATADIR}/${PART}.properties.datatypes.counts.unsorted.${SORTED_KGTK} \
      --columns-to-lift node1 \
      --input-file-is-presorted \
      --label-file ${DATADIR}/labels.en.${SORTED_KGTK} \
      --label-file-is-presorted \
-     / sort2 ${VERBOSE} --use-mgzip=$USE_MGZIP --pure-python \
+     --output-file ${DATADIR}/${PART}.properties.datatypes.counts.lifted.${SORTED_KGTK}
+
+kgtk ${KGTK_FLAGS} \
+     sort2 ${VERBOSE} --use-mgzip=$USE_MGZIP --pure-python \
+     --input-file ${DATADIR}/${PART}.properties.datatypes.counts.lifted.${SORTED_KGTK} \
      --columns node2 node1 label \
      --output-file ${DATADIR}/${PART}.properties.datatypes.counts.${SORTED_KGTK}
