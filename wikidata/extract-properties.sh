df848c50864b2df47dd37cedb8dff82f9e850078
Craig Milo Rogers <rogers@isi.edu>
2020-11-24 17:58:43 -0800

    Include the site qualifiers in the all file.  Shorten some patterns.  Fix an extension.

--- wikidata/extract-properties.sh
+++ wikidata/extract-properties.sh
@@ -5,10 +5,10 @@ source common.sh
 echo -e "\nExtract claims with a property in the node1 column."
 kgtk $KGTK_FLAGS filter $VERBOSE --use-mgzip=$USE_MGZIP --regex\
      --input-file $DATADIR/claims.$SORTED_KGTK \
-     -p '^P.*$ ;;' -o $DATADIR/claims.properties.$SORTED_KGTK
+     -p '^P ;;' -o $DATADIR/claims.properties.$SORTED_KGTK
 
 echo -e "\nExtract the qualifiers for claims with a property in node1."
 kgtk $KGTK_FLAGS filter $VERBOSE --use-mgzip=$USE_MGZIP --regex \
      --input-file $DATADIR/qualifiers.$SORTED_KGTK \
-    -p '^P.*$ ;;' -o $DATADIR/qualifiers.properties.$SORTED_KGTK
+    -p '^P ;;' -o $DATADIR/qualifiers.properties.$SORTED_KGTK
 
