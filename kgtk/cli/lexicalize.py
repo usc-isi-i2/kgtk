0a471900f52d9c33cbfc7df7139139ddd37441cd
Craig Milo Rogers <rogers@isi.edu>
2020-12-08 17:04:20 -0800

    Starting the framework for the lexicalize command.

---
+++ kgtk/cli/lexicalize.py
@@ -0,0 +1,101 @@
+# text embedding.
+#
+# TODO: Use KgtkReader to read the property values file.
+#
+# TODO: Provide seperate KgtkReader options (with fallback) for
+# property labels, property values, and the main input files
+# read by EmbeddingVector in "gt/embeddng_utils.py".
+#
+# TODO: Convert EmbeddingVector to use KgtkFormat and KgtkWriter.
+#
+from argparse import Namespace, SUPPRESS
+import typing
+
+from kgtk.cli_argparse import KGTKArgumentParser, KGTKFiles
+
+DEFAULT_LABEL_PROPERTIES: typing.List[str] = [ "label" ]
+DEFAULT_DESCRIPTION_PROPERTIES: typing.List[str] = [ "description" ]
+DEFAULT_ISA_PROPERTIES: typing.List[str] = [ "P21", "P31", "P39", "P106", "P279" ]
+DEFAULT_HAS_PROPERTIES: typing.List[str] = [ ]
+DEFAULT_PROPERTY_VALUES: typing.List[str] = [ "P17" ]
+DEFAULT_METADATA_PROPERTIES: typing.List[str] = [ "label", "P31"]
+
+def parser():
+    return {
+        'help': """Produce sentences from a KGTK file."""
+    }
+
+
+def add_arguments_extended(parser: KGTKArgumentParser, parsed_shared_args: Namespace ):
+
+    from kgtk.io.kgtkreader import KgtkReader, KgtkReaderOptions
+    from kgtk.utils.argparsehelpers import optional_bool
+    from kgtk.value.kgtkvalueoptions import KgtkValueOptions
+
+    _expert: bool = parsed_shared_args._expert
+
+    parser.add_input_file()
+    parser.add_output_file()
+
+    parser.add_argument("--label-properties", dest="label_properties", nargs="*",
+                        help="The label properties. (default=%s)" % repr(DEFAULT_LABEL_PROPERTIES))
+
+    KgtkReader.add_debug_arguments(parser, expert=False)
+    KgtkReaderOptions.add_arguments(parser, mode_options=True, expert=False)
+    KgtkValueOptions.add_arguments(parser, expert=False)
+
+
+def run(input_file: KGTKFiles,
+        output_file: KGTKFiles,
+
+        label_properties: typing.Optional[typing.List[str]],
+
+        errors_to_stdout: bool = False,
+        errors_to_stderr: bool = True,
+        show_options: bool = False,
+        verbose: bool = False,
+        very_verbose: bool = False,
+
+        **kwargs # Whatever KgtkFileOptions and KgtkValueOptions want.
+)->int:
+        
+    # import modules locally
+    from pathlib import Path
+    import sys
+    
+    from kgtk.exceptions import KGTKException
+
+    from kgtk.io.kgtkreader import KgtkReader, KgtkReaderOptions
+    from kgtk.io.kgtkwriter import KgtkWriter
+    from kgtk.value.kgtkvalueoptions import KgtkValueOptions
+
+    input_kgtk_file: Path = KGTKArgumentParser.get_input_file(input_file)
+    output_kgtk_file: Path = KGTKArgumentParser.get_output_file(output_file)
+
+    # Select where to send error messages, defaulting to stderr.
+    error_file: typing.TextIO = sys.stdout if errors_to_stdout else sys.stderr
+
+    # Build the option structures.
+    reader_options: KgtkReaderOptions = KgtkReaderOptions.from_dict(kwargs)
+    value_options: KgtkValueOptions = KgtkValueOptions.from_dict(kwargs)
+
+    if label_properties is None:
+        label_properties = DEFAULT_LABEL_PROPERTIES
+
+    # Show the final option structures for debugging and documentation.
+    if show_options:
+        print("--input-file=%s" % str(input_kgtk_file), file=error_file, flush=True)
+        print("--output-file=%s" % str(output_kgtk_file), file=error_file, flush=True)
+
+        if len(label_properties) > 0:
+            print("--label-properties %s" % " ".join(label_properties), file=error_file, flush=True)
+
+        reader_options.show(out=error_file)
+        value_options.show(out=error_file)
+        print("=======", file=error_file, flush=True)
+
+    try:
+        return 0
+
+    except Exception as e:
+        raise KGTKException(str(e))
