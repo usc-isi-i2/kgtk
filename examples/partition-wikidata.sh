f7e4dd3b0311c845fa058b270f95aaeac01d94d0
Craig Milo Rogers <rogers@isi.edu>
2020-12-14 17:46:58 -0800

    Improve the documentation frames.

--- examples/partition-wikidata.sh
+++ examples/partition-wikidata.sh
@@ -4,4 +4,7 @@ papermill \
     --progress-bar \
     --log-output --log-level DEBUG \
     partition-wikidata.ipynb \
+    -p wikidata_input_path ./all.tsv.gz \
+    -p wikidata_parts_path ./parts \
+    -p temp_folder_path ./parts/temp \
     |& tee partition-wikidata.log
