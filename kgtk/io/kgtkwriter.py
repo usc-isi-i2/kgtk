8a09616c8f35c1048fed5af2479d4b76e1ec56b9
Craig Milo Rogers <rogers@isi.edu>
2021-03-04 15:01:47 -0800

    Simplify quoting vertical bars in md output.

--- kgtk/io/kgtkwriter.py
+++ kgtk/io/kgtkwriter.py
@@ -534,7 +534,7 @@ class KgtkWriter(KgtkBase):
         value: str
         for value in values:
             linebuf.append(" ")
-            linebuf.append("\\|".join(value.replace("\\", "\\\\").split("|")))
+            linebuf.append(value.replace("\\", "\\\\").replace("|", "\\|"))
             linebuf.append(" |")
         return "".join(linebuf)
 
