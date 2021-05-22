fb0a31d67b4b7764799db882a4b3c313dc34b54f
Craig Milo Rogers <rogers@isi.edu>
2021-05-21 17:46:15 -0700

    Pipe stdout and stderr to the log file.

--- wikidata/extract-properties.sh
+++ wikidata/extract-properties.sh
@@ -6,11 +6,11 @@ echo -e "\nExtract claims with a property in the node1 column."
 kgtk $KGTK_FLAGS filter $VERBOSE --use-mgzip=$USE_MGZIP --regex\
      --input-file $DATADIR/claims.$SORTED_KGTK \
      -p '^P ;;' -o $DATADIR/claims.properties.$SORTED_KGTK \
-     | tee ${LOGDIR}/claims.properties.log
+     |& tee ${LOGDIR}/claims.properties.log
 
 echo -e "\nExtract the qualifiers for claims with a property in node1."
 kgtk $KGTK_FLAGS filter $VERBOSE --use-mgzip=$USE_MGZIP --regex \
      --input-file $DATADIR/qualifiers.$SORTED_KGTK \
      -p '^P ;;' -o $DATADIR/qualifiers.properties.$SORTED_KGTK \
-     | tee ${LOGDIR}/qualifiers.properties.log
+     |& tee ${LOGDIR}/qualifiers.properties.log
 
