ff2ddcdc606ab54c7d481fabcb84e4a25b8ea9e1
Craig Milo Rogers <rogers@isi.edu>
2021-10-03 11:29:38 -0700

    Add documentation.

--- kgtk_function.py
+++ kgtk_function.py
@@ -17,6 +17,77 @@ def kgtk(arg1: typing.Union[str, pandas.DataFrame],
          kgtk_command: str = os.getenv("KGTK_KGTK_COMMAND", "kgtk"),
          bash_command: str = os.getenv("KGTK_BASH_COMMAND", "bash"),
          )->typing.Optional[pandas.DataFrame]:
+    """This function simplifies using KGTK commands in a Jupyter Lab environment.
+
+    Invocation
+    ==========
+
+    kgtk("pipeline")
+
+    Execute the command pipeline.  The results are printed, displayed, or
+    returned as a Pandas DataFrame.
+
+    kgtk(df, "pipeline")
+
+    The `df` in the call is a Pandas DataFrame, which is converted to KGTK
+    format and passed to the pipeline as standard input. The results are
+    printed, displayed, or returned as a Pandas DataFrame.
+
+    Optional Parameters
+    ======== ==========
+
+    df=DF (default None)
+    
+    This is an alternat method for specifying an input DataFrame.
+
+    auto_display_md=True/False (default False)
+
+    This parameter controls the processing of MarkDown output.  See below.
+
+    auto_display_json=True/False (default True)
+
+    This parameter controls the processing of JSON output.  See below.
+
+    auto_display_html=True/False (default True)
+
+    This parameter controls the processing of HTML output.  See below.
+
+    kgtk_command=CMD (default 'kgtk')
+
+    This parameter specifies the kgtk shell command.
+
+    bash_command=CMD (default 'bash')
+
+    This parameter specifies the name of the shell interpreter.
+
+    Output Processing
+    ====== =========
+
+    If the output of the pipeline is MarkDown format (typically by ending the
+    pipeline in `... / md` or `... /table`, identified as starting with `|`,
+    the output will be printed.  However, if `auto_display_md=True` is passed
+    in the `kgtk(...)` call, or if the envar `KGTK_AUTO_DISPLAY_MD` is set to
+    `true`, then the MarkDown will be displayed using
+    `display(Markdown(output))`.
+
+    If the output of the pipeline is in JSON format (`--output-format JSON`),
+    identified as starting with `[`, the output will be displayed with
+    `display(JSON(output))`.  However, if `kgtk(... auto_display_json=False)`
+    or if the envar `KGTK_AUTO_DISPLAY_JSON` set to `false`, then the output
+    will be printed.
+
+    If the output of the pipeline is in HTML format (`--output-format HTML` or
+    `kgtk("... /html")`), identified by starting with `<!DOCTYPE html>`, the
+    output will be displayed with `display(HTML(output))`.  However, if
+    `kgtk(... auto_display_json=False)` or if the envar
+    `KGTK_AUTO_DISPLAY_HTML` set to `false`, then the output will be printed.
+
+    If the output starts with anything other than `|`, `[`, or `<!DOCTYPE
+    html>`, then the output is assumed to be in KGTK format.  It is converted
+    to a Pandas DataFrame, which is returned to the caller.
+
+    """
+
     in_df: typing.Optional[pandas.DataFrame] = None
     pipeline: str
     if isinstance(arg1, str):
