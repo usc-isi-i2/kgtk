0fc843abc29fd4618285de8869f7a3f19aa13895
Craig Milo Rogers <rogers@isi.edu>
2021-10-04 08:12:17 -0700

    Fix a typo.  Process stderr.  Document.

--- kgtk_function.py
+++ kgtk_function.py
@@ -60,33 +60,37 @@ def kgtk(arg1: typing.Union[str, pandas.DataFrame],
 
     This parameter specifies the name of the shell interpreter.
 
-    Output Processing
-    ====== =========
+    Standard Output Processing
+    ======== ====== =========
 
-    If the output of the pipeline is MarkDown format (typically by ending the
-    pipeline in `... / md` or `... /table`, identified as starting with `|`,
-    the output will be printed.  However, if `auto_display_md=True` is passed
-    in the `kgtk(...)` call, or if the envar `KGTK_AUTO_DISPLAY_MD` is set to
-    `true`, then the MarkDown will be displayed using
-    `display(Markdown(output))`.
+    If the standard output of the pipeline is MarkDown format (typically by
+    ending the pipeline in `... / md` or `... /table`, identified as starting
+    with `|`, the output will be printed by default.  However, if
+    `auto_display_md=True` is passed in the `kgtk(...)` call, or if the envar
+    `KGTK_AUTO_DISPLAY_MD` is set to `true`, then the MarkDown will be
+    displayed using `display(Markdown(output))`.
 
-    If the output of the pipeline is in JSON format (`--output-format JSON`),
+    If the standard output of the pipeline is in JSON format (`--output-format JSON`),
     identified as starting with `[` or '{', the output will be displayed with
-    `display(JSON(output))`.  However, if `kgtk(... auto_display_json=False)`
-    or if the envar `KGTK_AUTO_DISPLAY_JSON` set to `false`, then the output
-    will be printed.
-
-    If the output of the pipeline is in HTML format (`--output-format HTML` or
-    `kgtk("... /html")`), identified by starting with `<!DOCTYPE html>`, the
-    output will be displayed with `display(HTML(output))`.  However, if
+    `display(JSON(output))` by default.  However, if
     `kgtk(... auto_display_json=False)` or if the envar
+    `KGTK_AUTO_DISPLAY_JSON` set to `false`, then the output will be printed.
+
+    If the standard output of the pipeline is in HTML format (`--output-format HTML` or
+    `kgtk("... /html")`), identified by starting with `<!DOCTYPE html>`, the
+    output will be displayed with `display(HTML(output))` by default.
+    However, if `kgtk(... auto_display_json=False)` or if the envar
     `KGTK_AUTO_DISPLAY_HTML` set to `false`, then the output will be printed.
 
-    If the output starts with anything other than `|`, `[`, or `<!DOCTYPE
+    If the standard output starts with anything other than `|`, `[`, or `<!DOCTYPE
     html>`, then the output is assumed to be in KGTK format.  It is converted
     to a Pandas DataFrame, which is returned to the caller.
 
-    TODO: Capture standard error from the subprocess and print it on standard output.
+    Error Output Processing
+    ===== ====== ==========
+
+    Error output is printed after standard output is printed or displayed.
+
     """
 
     MD_SIGIL: str = "|"
@@ -125,9 +129,10 @@ def kgtk(arg1: typing.Union[str, pandas.DataFrame],
                               )
 
     outbuf: StringIO = StringIO()
+    errbuf: StringIO = StringIO()
 
     sh_bash = sh.Command(bash_command)
-    sh_bash("-c", pipeline, _in=in_tsv, _out=outbuf, _err=sys.stderr)
+    sh_bash("-c", pipeline, _in=in_tsv, _out=outbuf, _err=errbuf)
 
     output: str = outbuf.getvalue()
 
@@ -139,7 +144,7 @@ def kgtk(arg1: typing.Union[str, pandas.DataFrame],
         else:
             print(output)
 
-    elif output.startswith(JSON_SIGIL) or output.startswith(JSON_MAP_SIGIL):
+    elif output.startswith(JSON_SIGIL) or output.startswith(JSONL_MAP_SIGIL):
         if auto_display_json:
             display(JSON(json.loads(output)))
         else:
@@ -157,4 +162,9 @@ def kgtk(arg1: typing.Union[str, pandas.DataFrame],
         result = pandas.read_csv(outbuf, sep='\t')
 
     outbuf.close()
+
+    # Any error messages? If so, print the at the end.
+    if errbuf.len > 0:
+        print(errbuf.getvalue())
+
     return result
