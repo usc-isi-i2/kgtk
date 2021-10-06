dc8c28e6416c506fe1336f5dcf5fd4eb23b7743d
Craig Milo Rogers <rogers@isi.edu>
2021-10-06 15:35:47 -0700

    Change the options to make it easier to understand what they do.  The new option set is incompatible with the ld options.

--- kgtk/cli/graph_statistics.py
+++ kgtk/cli/graph_statistics.py
@@ -13,7 +13,11 @@ from kgtk.cli_argparse import KGTKArgumentParser, KGTKFiles
 def parser():
     return {
         'help': 'Import a TSV or CSV file in Graph-tool, optionally generating various statistics ' +
-        '(pagerank, in-degrees, out-degrees, degree distribution, and hits).'
+        '(pagerank, in-degrees, out-degrees, degree distribution, and hits).  Two different output ' +
+        'files are generated: a log file, containing a combination of TSV data nd plain text records, ' +
+        'and a KGTK file. Summary statistics are directed to ' +
+        'the text log file, while detailed statistics (in-degrees, out-degrees, pagerank) are directed ' +
+        'to the KGTK output file.'
     }
 
 
@@ -39,39 +43,53 @@ def add_arguments_extended(parser: KGTKArgumentParser, parsed_shared_args: Names
                         '\n(default=%(default)s)',
                         type=optional_bool, nargs='?', const=True, default=False, metavar='True|False')
 
-    parser.add_argument('--histogram', dest='compute_degrees_histogram',
-                        help='Whether or not to compute degree distribution and output it to the log file. (default=%(default)s)',
-                        type=optional_bool, nargs='?', const=True, default=False, metavar='True|False')
-
-    parser.add_argument('--top-relations', dest='compute_top_relations',
-                        help='Whether or not to compute top relations and output them to the log file. (default=%(default)s)',
-                        type=optional_bool, nargs='?', const=True, default=True, metavar='True|False')
-
-    parser.add_argument('--output-degrees', dest='output_degrees',
-                        help='Whether or not to output degree edges. (default=%(default)s)',
-                        type=optional_bool, nargs='?', const=True, default=True, metavar='True|False')
-
-    parser.add_argument('--output-properties', dest='output_properties',
-                        help='Whether or not to output property edges. (default=%(default)s)',
-                        type=optional_bool, nargs='?', const=True, default=True, metavar='True|False')
-
-    parser.add_argument('--pagerank', dest='compute_pagerank',
-                        help='Whether or not to compute PageRank centraility and output it to the log file. ' +
+    parser.add_argument('--compute-pagerank', dest='compute_pagerank',
+                        help='Whether or not to compute the PageRank property. ' +
                         '\nNote: --undirected improves the pagerank calculation. ' +
                         'If you want both pagerank and in/out-degrees, you should make two runs. ' +
                         '\n(default=%(default)s)',
-                        type=optional_bool, nargs='?', const=True, default=False, metavar='True|False')
+                        type=optional_bool, nargs='?', const=True, default=True, metavar='True|False')
                         
-    parser.add_argument('--hits', dest='compute_hits',
-                        help='Whether or not to compute HITS centraility and output it to the log file. (default=%(default)s)',
+    parser.add_argument('--compute-hits', dest='compute_hits',
+                        help='Whether or not to compute the HITS properties. (default=%(default)s)',
+                        type=optional_bool, nargs='?', const=True, default=True, metavar='True|False')
+
+    parser.add_argument('--output-statistics-only', dest='output_statistics_only',
+                        help='If this option is set, write only the statistics edges to the primary output file. ' +
+                        'Else, write both the statistics and the original graph. (default=%(default)s',
                         type=optional_bool, nargs='?', const=True, default=False, metavar='True|False')
 
-    parser.add_argument('--log', action='store', type=str, dest='log_file',
+    parser.add_argument('--output-degrees', dest='output_degrees',
+                        help='Whether or not to write degree edges to the primary output file. (default=%(default)s)',
+                        type=optional_bool, nargs='?', const=True, default=True, metavar='True|False')
+
+    parser.add_argument('--output-properties', dest='output_properties',
+                        help='Whether or not to write property edges (e.g., pagerank, HITS) to the primary output file. (default=%(default)s)',
+                        type=optional_bool, nargs='?', const=True, default=True, metavar='True|False')
+
+    parser.add_argument('--log-file', action='store', type=str, dest='log_file',
                         help='Summary file for the global statistics of the graph.', default='./summary.txt')
 
-    parser.add_argument('--statistics-only', dest='statistics_only',
-                        help='If this flag is set, output only the statistics edges. Else, append the statistics to the original graph. (default=%(default)s',
-                        type=optional_bool, nargs='?', const=True, default=False, metavar='True|False')
+    parser.add_argument('--log-top-relations', dest='log_top_relations',
+                        help='Whether or not to compute top relations and output them to the log file. (default=%(default)s)',
+                        type=optional_bool, nargs='?', const=True, default=True, metavar='True|False')
+
+    parser.add_argument('--log-degrees-histogram', dest='log_degrees_histogram',
+                        help='Whether or not to compute degree distribution and output it to the log file. (default=%(default)s)',
+                        type=optional_bool, nargs='?', const=True, default=True, metavar='True|False')
+
+    parser.add_argument('--log-top-pageranks', dest='log_top_pageranks',
+                        help='Whether or not to output PageRank centrality top-n to the log file. ' +
+                        '\n(default=%(default)s)',
+                        type=optional_bool, nargs='?', const=True, default=True, metavar='True|False')
+                        
+    parser.add_argument('--log-top-hits', dest='log_top_hits',
+                        help='Whether or not to output the top-n HITS to the log file. (default=%(default)s)',
+                        type=optional_bool, nargs='?', const=True, default=True, metavar='True|False')
+
+    parser.add_argument('--log-top-n', action='store', dest='top_n',
+                        default=5, type=int,
+                        help='Number of top centrality nodes to write to the log file. (default=%(default)d)')
 
     parser.add_argument('--vertex-in-degree-property', action='store', dest='vertex_in_degree',
                         default='vertex_in_degree',
@@ -90,7 +108,7 @@ def add_arguments_extended(parser: KGTKArgumentParser, parsed_shared_args: Names
     parser.add_argument('--page-rank-property', action='store', dest='vertex_pagerank',
                         default='vertex_pagerank',
                         help='Label for pank rank property. (default=%(default)s)')
-
+    
     parser.add_argument('--vertex-hits-authority-property', action='store', dest='vertex_auth',
                         default='vertex_auth',
                         help='Label for edge: vertext hits authority. (default=%(default)s)')
@@ -98,10 +116,6 @@ def add_arguments_extended(parser: KGTKArgumentParser, parsed_shared_args: Names
     parser.add_argument('--vertex-hits-hubs-property', action='store', dest='vertex_hubs',
                         default='vertex_hubs',
                         help='Label for edge: vertex hits hubs. (default=%(default)s)')
-
-    parser.add_argument('--print-top-n', action='store', dest='top_n',
-                        default=5, type=int,
-                        help='Number of top centrality nodes to print. (default=%(default)d)')
 
     KgtkReader.add_debug_arguments(parser, expert=_expert)
     KgtkReaderOptions.add_arguments(parser, mode_options=True, expert=_expert)
@@ -111,20 +125,25 @@ def run(input_file: KGTKFiles,
         output_file: KGTKFiles,
         
         undirected: bool,
-        compute_top_relations: bool,
-        compute_degrees_histogram: bool,
         compute_pagerank: bool,
         compute_hits: bool,
+
+        output_statistics_only: bool,
         output_degrees: bool,
         output_properties: bool,
+        
         log_file: str,
-        statistics_only: bool,
+        log_degrees_histogram: bool,
+        log_top_relations: bool,
+        log_top_pageranks: bool,
+        log_top_hits: bool,
+        top_n: int,
+
         vertex_in_degree: str,
         vertex_out_degree: str,
         vertex_pagerank: str,
         vertex_auth: str,
         vertex_hubs: str,
-        top_n: int,
 
         errors_to_stdout: bool,
         errors_to_stderr: bool,
@@ -145,6 +164,12 @@ def run(input_file: KGTKFiles,
     from kgtk.io.kgtkreader import KgtkReader, KgtkReaderOptions
     from kgtk.io.kgtkwriter import KgtkWriter
     from kgtk.value.kgtkvalueoptions import KgtkValueOptions
+
+    if log_top_pageranks and not compute_pagerank:
+        raise KGTKException("--log-pagerank requires --compute-pagerank")
+
+    if log_top_hits and not compute_hits:
+        raise KGTKException("--log-top-hits requires --compute-hits")
 
     v_prop_dict = {
         'vertex_pagerank': vertex_pagerank,
@@ -195,44 +220,17 @@ def run(input_file: KGTKFiles,
         G2 = load_graph_from_kgtk(kr, directed=not undirected, ecols=(sub, obj), verbose=verbose, out=error_file)
         if verbose:
             print('graph loaded! It has %d nodes and %d edges.' % (G2.num_vertices(), G2.num_edges()), file=error_file, flush=True)
+        kr.close()
 
-        with open(log_file, 'w') as writer:
-            writer.write('graph loaded! It has %d nodes and %d edges\n' % (G2.num_vertices(), G2.num_edges()))
-            if compute_top_relations:
-                writer.write('\n###Top relations:\n')
-                for rel, freq in gtanalysis.get_topN_relations(G2, pred_property=predicate):
-                    writer.write('%s\t%d\n' % (rel, freq))
+        if compute_pagerank:
+            if verbose:
+                print('Computing pagerank.', file=error_file, flush=True)
+            v_pr = G2.new_vertex_property('float')
+            centrality.pagerank(G2, prop=v_pr)
+            G2.properties[('v', 'vertex_pagerank')] = v_pr
 
-            if compute_degrees_histogram:
-                writer.write('\n###Degrees:\n')
-                for direction in directions:
-                    degree_data = gtanalysis.compute_node_degree_hist(G2, direction)
-                    max_degree = len(degree_data) - 1
-                    mean_degree, std_degree = gtanalysis.compute_avg_node_degree(G2, direction)
-                    writer.write(
-                        '%s degree stats: mean=%f, std=%f, max=%d\n' % (direction, mean_degree, std_degree, max_degree))
-
-            if compute_pagerank:
-                writer.write('\n###PageRank\n')
-                v_pr = G2.new_vertex_property('float')
-                centrality.pagerank(G2, prop=v_pr)
-                G2.properties[('v', 'vertex_pagerank')] = v_pr
-                writer.write('Max pageranks\n')
-                result = gtanalysis.get_topn_indices(G2, 'vertex_pagerank', top_n, id_col)
-                for n_id, n_label, pr in result:
-                    writer.write('%s\t%s\t%f\n' % (n_id, n_label, pr))
-
-            if compute_hits:
-                writer.write('\n###HITS\n')
-                hits_eig, G2.vp['vertex_hubs'], G2.vp['vertex_auth'] = gtanalysis.compute_hits(G2)
-                writer.write('HITS hubs\n')
-                main_hubs = gtanalysis.get_topn_indices(G2, 'vertex_hubs', top_n, id_col)
-                for n_id, n_label, hubness in main_hubs:
-                    writer.write('%s\t%s\t%f\n' % (n_id, n_label, hubness))
-                writer.write('HITS auth\n')
-                main_auth = gtanalysis.get_topn_indices(G2, 'vertex_auth', top_n, id_col)
-                for n_id, n_label, authority in main_auth:
-                    writer.write('%s\t%s\t%f\n' % (n_id, n_label, authority))
+        if compute_hits:
+            hits_eig, G2.vp['vertex_hubs'], G2.vp['vertex_auth'] = gtanalysis.compute_hits(G2)
 
         kw: KgtkWriter = KgtkWriter.open(output_columns,
                                      output_kgtk_file,
@@ -243,7 +241,7 @@ def run(input_file: KGTKFiles,
                                      verbose=verbose,
                                      very_verbose=very_verbose)
 
-        if not statistics_only:
+        if not output_statistics_only:
             id_count = 0
             for e in G2.edges():
                 sid, oid = e
@@ -251,14 +249,15 @@ def run(input_file: KGTKFiles,
                 kw.write([G2.vp[id_col][sid], lbl, G2.vp[id_col][oid], '{}-{}-{}'.format(G2.vp[id_col][sid], lbl, id_count)])
                 id_count += 1
 
-        if output_degrees:
+        if output_degrees or output_properties:
             id_count = 0
             for v in G2.vertices():
                 v_id = G2.vp[id_col][v]
-                kw.write([v_id, vertex_in_degree, str(v.in_degree()), '{}-{}-{}'.format(v_id, vertex_in_degree, id_count)])
-                id_count += 1
-                kw.write([v_id, vertex_out_degree, str(v.out_degree()), '{}-{}-{}'.format(v_id, vertex_out_degree, id_count)])
-                id_count += 1
+                if output_degrees:
+                    kw.write([v_id, vertex_in_degree, str(v.in_degree()), '{}-{}-{}'.format(v_id, vertex_in_degree, id_count)])
+                    id_count += 1
+                    kw.write([v_id, vertex_out_degree, str(v.out_degree()), '{}-{}-{}'.format(v_id, vertex_out_degree, id_count)])
+                    id_count += 1
 
                 if output_properties:
                     for vprop in G2.vertex_properties.keys():
@@ -268,7 +267,40 @@ def run(input_file: KGTKFiles,
                         id_count += 1
 
         kw.close()
-        kr.close()
+
+        with open(log_file, 'w') as writer:
+            writer.write('graph loaded! It has %d nodes and %d edges\n' % (G2.num_vertices(), G2.num_edges()))
+            if log_top_relations:
+                writer.write('\n###Top relations:\n')
+                for rel, freq in gtanalysis.get_topN_relations(G2, pred_property=predicate):
+                    writer.write('%s\t%d\n' % (rel, freq))
+
+            if log_degrees_histogram:
+                writer.write('\n###Degrees:\n')
+                for direction in directions:
+                    degree_data = gtanalysis.compute_node_degree_hist(G2, direction)
+                    max_degree = len(degree_data) - 1
+                    mean_degree, std_degree = gtanalysis.compute_avg_node_degree(G2, direction)
+                    writer.write(
+                        '%s degree stats: mean=%f, std=%f, max=%d\n' % (direction, mean_degree, std_degree, max_degree))
+
+            if log_top_pageranks:
+                writer.write('\n###PageRank\n')
+                writer.write('Max pageranks\n')
+                result = gtanalysis.get_topn_indices(G2, 'vertex_pagerank', top_n, id_col)
+                for n_id, n_label, pr in result:
+                    writer.write('%s\t%s\t%f\n' % (n_id, n_label, pr))
+
+            if log_top_hits:
+                writer.write('\n###HITS\n')
+                writer.write('HITS hubs\n')
+                main_hubs = gtanalysis.get_topn_indices(G2, 'vertex_hubs', top_n, id_col)
+                for n_id, n_label, hubness in main_hubs:
+                    writer.write('%s\t%s\t%f\n' % (n_id, n_label, hubness))
+                writer.write('HITS auth\n')
+                main_auth = gtanalysis.get_topn_indices(G2, 'vertex_auth', top_n, id_col)
+                for n_id, n_label, authority in main_auth:
+                    writer.write('%s\t%s\t%f\n' % (n_id, n_label, authority))
 
     except Exception as e:
         raise KGTKException('Error: ' + str(e))
