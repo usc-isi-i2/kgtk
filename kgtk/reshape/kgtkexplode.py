3b2a10a765dd51f4bec98973ab152a74775c3e5c
Craig Milo Rogers <rogers@isi.edu>
2022-02-01 16:30:37 -0800

    Close the KGTK files.

--- kgtk/reshape/kgtkexplode.py
+++ kgtk/reshape/kgtkexplode.py
@@ -54,142 +54,150 @@ class KgtkExplode(KgtkFormat):
     very_verbose: bool = attr.ib(validator=attr.validators.instance_of(bool), default=False)
 
     def process(self):
-        if len(self.column_name) == 0:
-            raise ValueError("The name of the column to explode is empty.")
+        kr: typing.Optional[KgtkReader] = None
+        ew: typing.Optional[KgtkWriter] = None
 
-        selected_field_names: typing.List[str] = [ ]
-        field_name: str
+        try:
+            if len(self.column_name) == 0:
+                raise ValueError("The name of the column to explode is empty.")
 
-        if self.type_names is not None:
-            if self.verbose:
-                print("Validate the names of the data types to extract.", file=self.error_file, flush=True)
-            type_name: str
-            for type_name in self.type_names:
-                if type_name not in KgtkValueFields.DEFAULT_DATA_TYPE_FIELDS:
-                    raise ValueError("Unknown data type name '%s'." % type_name)
-                # Merge this KGTK data type's fields into the list of selected fields:
-                for field_name in KgtkValueFields.DEFAULT_DATA_TYPE_FIELDS[type_name]:
+            selected_field_names: typing.List[str] = [ ]
+            field_name: str
+
+            if self.type_names is not None:
+                if self.verbose:
+                    print("Validate the names of the data types to extract.", file=self.error_file, flush=True)
+                type_name: str
+                for type_name in self.type_names:
+                    if type_name not in KgtkValueFields.DEFAULT_DATA_TYPE_FIELDS:
+                        raise ValueError("Unknown data type name '%s'." % type_name)
+                    # Merge this KGTK data type's fields into the list of selected fields:
+                    for field_name in KgtkValueFields.DEFAULT_DATA_TYPE_FIELDS[type_name]:
+                        if field_name not in selected_field_names:
+                            selected_field_names.append(field_name)
+
+            if self.field_names is not None:
+                # Forget the fields selected above, choose these instead:
+                selected_field_names = [ ]
+                if self.verbose:
+                    print("Validate the names of the fields to extract.", file=self.error_file, flush=True)
+                for field_name in self.field_names:
+                    if field_name not in KgtkValueFields.FIELD_NAMES:
+                        raise ValueError("Unknown field name '%s'." % field_name)
+                    # Merge this field into the list of selected fields:
                     if field_name not in selected_field_names:
                         selected_field_names.append(field_name)
 
-        if self.field_names is not None:
-            # Forget the fields selected above, choose these instead:
-            selected_field_names = [ ]
+            if len(selected_field_names) == 0:
+                raise ValueError("The list of fields to explode is empty.")
+
+            # Open the input file.
             if self.verbose:
-                print("Validate the names of the fields to extract.", file=self.error_file, flush=True)
-            for field_name in self.field_names:
-                if field_name not in KgtkValueFields.FIELD_NAMES:
-                    raise ValueError("Unknown field name '%s'." % field_name)
-                # Merge this field into the list of selected fields:
-                if field_name not in selected_field_names:
-                    selected_field_names.append(field_name)
-
-        if len(selected_field_names) == 0:
-            raise ValueError("The list of fields to explode is empty.")
-
-        # Open the input file.
-        if self.verbose:
-            if self.input_file_path is not None:
-                print("Opening the input file: %s" % self.input_file_path, file=self.error_file, flush=True)
-            else:
-                print("Reading the input data from stdin", file=self.error_file, flush=True)
-
-        kr: KgtkReader =  KgtkReader.open(self.input_file_path,
-                                          error_file=self.error_file,
-                                          options=self.reader_options,
-                                          value_options = self.value_options,
-                                          verbose=self.verbose,
-                                          very_verbose=self.very_verbose,
-        )
-
-        if self.verbose:
-            print("Check that the source column '%s' is present." % self.column_name, file=self.error_file, flush=True)
-        if self.column_name not in kr.column_name_map:
-            raise ValueError("Column name '%s' not found in the input file." % self.column_name)
-        column_idx: int = kr.column_name_map[self.column_name]
-
-        if self.verbose:
-            print("Build the map of exploded columns and list of new column names", file=self.error_file, flush=True)
-        explosion: typing.MutableMapping[str, idx] = { }
-        column_names: typing.List[str] = kr.column_names.copy()
-        for field_name in selected_field_names:
-            exploded_name: str = self.prefix + field_name
-            if self.verbose:
-                print("Field '%s' becomes '%s'" % (field_name, exploded_name), file=self.error_file, flush=True)
-            if exploded_name in explosion:
-                raise ValueError("Field name '%s' is duplicated in the field list.")
-            if exploded_name in kr.column_names:
-                if self.overwrite_columns:
-                    existing_idx = kr.column_name_map[exploded_name]
-                    explosion[field_name] = existing_idx
-                    if self.verbose:
-                        print("Field '%s' is overwriting existing column '%s' (idx=%d)" % (field_name, exploded_name, existing_idx),
-                              file=self.error_file, flush=True)
+                if self.input_file_path is not None:
+                    print("Opening the input file: %s" % self.input_file_path, file=self.error_file, flush=True)
                 else:
-                    raise ValueError("Exploded column '%s' already exists and not allowed to overwrite" % exploded_name)
-            else:
-                column_names.append(exploded_name)
-                exploded_idx: int = len(column_names) - 1
-                explosion[field_name] = exploded_idx
-                if self.verbose:
-                    print("Field '%s' becomes new column '%s' (idx=%d)" % (field_name, exploded_name, exploded_idx), file=self.error_file, flush=True)
-        new_column_count: int = len(column_names) - kr.column_count
-        if self.verbose:
-            print("%d columns + %d columns = %d columns" % (kr.column_count, new_column_count, len(column_names)))
-            print("Explosion length: %d" % len(explosion))
-                
-        # Open the output file.
-        ew: KgtkWriter = KgtkWriter.open(column_names,
-                                         self.output_file_path,
-                                         mode=kr.mode,
-                                         output_format=self.output_format,
-                                         require_all_columns=False,
-                                         prohibit_extra_columns=True,
-                                         fill_missing_columns=True,
-                                         gzip_in_parallel=False,
-                                         verbose=self.verbose,
-                                         very_verbose=self.very_verbose)        
-        
-        if self.verbose:
-            print("Expanding records from %s" % self.input_file_path, file=self.error_file, flush=True)
-        input_line_count: int = 0
-        output_line_count: int = 0;
+                    print("Reading the input data from stdin", file=self.error_file, flush=True)
 
-        row: typing.List[str]
-        for row in kr:
-            input_line_count += 1
+            kr = KgtkReader.open(self.input_file_path,
+                                 error_file=self.error_file,
+                                 options=self.reader_options,
+                                 value_options = self.value_options,
+                                 verbose=self.verbose,
+                                 very_verbose=self.very_verbose,
+                                 )
 
-            # Parse the value for the colummn being exploded:
-            item_to_explode: str = row[column_idx]
-            value: KgtkValue = KgtkValue(item_to_explode, options=self.value_options, parse_fields=True)
-            value.validate()
-            if not value.is_valid():
-                if self.verbose:
-                    print("Not exploding invalid item '%s' in input line %d" % (item_to_explode, input_line_count), file=self.error_file, flush=True)
-                ew.write(row) # This will be filled to the proper length
-                output_line_count += 1
-                continue
+            if self.verbose:
+                print("Check that the source column '%s' is present." % self.column_name, file=self.error_file, flush=True)
+            if self.column_name not in kr.column_name_map:
+                raise ValueError("Column name '%s' not found in the input file." % self.column_name)
+            column_idx: int = kr.column_name_map[self.column_name]
 
-            if self.expand_list and value.is_list():
+            if self.verbose:
+                print("Build the map of exploded columns and list of new column names", file=self.error_file, flush=True)
+            explosion: typing.MutableMapping[str, idx] = { }
+            column_names: typing.List[str] = kr.column_names.copy()
+            for field_name in selected_field_names:
+                exploded_name: str = self.prefix + field_name
                 if self.verbose:
-                    print("Expanding a list: '%s'" % item_to_explode, file=self.error_file, flush=True)
-                subvalue: KgtkValue
-                for subvalue in value.get_list_items():
-                    if self.very_verbose:
-                        print("Exploding '%s'" % subvalue.value)
-                    ew.write(self.explode(subvalue, row, explosion, new_column_count))
+                    print("Field '%s' becomes '%s'" % (field_name, exploded_name), file=self.error_file, flush=True)
+                if exploded_name in explosion:
+                    raise ValueError("Field name '%s' is duplicated in the field list.")
+                if exploded_name in kr.column_names:
+                    if self.overwrite_columns:
+                        existing_idx = kr.column_name_map[exploded_name]
+                        explosion[field_name] = existing_idx
+                        if self.verbose:
+                            print("Field '%s' is overwriting existing column '%s' (idx=%d)" % (field_name, exploded_name, existing_idx),
+                                  file=self.error_file, flush=True)
+                    else:
+                        raise ValueError("Exploded column '%s' already exists and not allowed to overwrite" % exploded_name)
+                else:
+                    column_names.append(exploded_name)
+                    exploded_idx: int = len(column_names) - 1
+                    explosion[field_name] = exploded_idx
+                    if self.verbose:
+                        print("Field '%s' becomes new column '%s' (idx=%d)" % (field_name, exploded_name, exploded_idx), file=self.error_file, flush=True)
+            new_column_count: int = len(column_names) - kr.column_count
+            if self.verbose:
+                print("%d columns + %d columns = %d columns" % (kr.column_count, new_column_count, len(column_names)))
+                print("Explosion length: %d" % len(explosion))
+
+            # Open the output file.
+            ew = KgtkWriter.open(column_names,
+                                 self.output_file_path,
+                                 mode=kr.mode,
+                                 output_format=self.output_format,
+                                 require_all_columns=False,
+                                 prohibit_extra_columns=True,
+                                 fill_missing_columns=True,
+                                 gzip_in_parallel=False,
+                                 verbose=self.verbose,
+                                 very_verbose=self.very_verbose)        
+
+            if self.verbose:
+                print("Expanding records from %s" % self.input_file_path, file=self.error_file, flush=True)
+            input_line_count: int = 0
+            output_line_count: int = 0;
+
+            row: typing.List[str]
+            for row in kr:
+                input_line_count += 1
+
+                # Parse the value for the colummn being exploded:
+                item_to_explode: str = row[column_idx]
+                value: KgtkValue = KgtkValue(item_to_explode, options=self.value_options, parse_fields=True)
+                value.validate()
+                if not value.is_valid():
+                    if self.verbose:
+                        print("Not exploding invalid item '%s' in input line %d" % (item_to_explode, input_line_count), file=self.error_file, flush=True)
+                    ew.write(row) # This will be filled to the proper length
                     output_line_count += 1
-            else:
-                if self.very_verbose:
-                    print("Exploding '%s'" % value.value)
-                ew.write(self.explode(value, row, explosion, new_column_count))
-                output_line_count += 1
+                    continue
 
-        if self.verbose:
-            print("Read %d records, wrote %d records." % (input_line_count, output_line_count), file=self.error_file, flush=True)
-        
-        ew.close()
-        kr.close()
+                if self.expand_list and value.is_list():
+                    if self.verbose:
+                        print("Expanding a list: '%s'" % item_to_explode, file=self.error_file, flush=True)
+                    subvalue: KgtkValue
+                    for subvalue in value.get_list_items():
+                        if self.very_verbose:
+                            print("Exploding '%s'" % subvalue.value)
+                        ew.write(self.explode(subvalue, row, explosion, new_column_count))
+                        output_line_count += 1
+                else:
+                    if self.very_verbose:
+                        print("Exploding '%s'" % value.value)
+                    ew.write(self.explode(value, row, explosion, new_column_count))
+                    output_line_count += 1
+
+            if self.verbose:
+                print("Read %d records, wrote %d records." % (input_line_count, output_line_count), file=self.error_file, flush=True)
+
+        finally:
+            if ew is not None:
+                ew.close()
+
+            if kr is not None:
+                kr.close()
 
     def explode(self, value: KgtkValue, row: typing.List[str], explosion: typing.Mapping[str, int], new_column_count: int)->typing.List[str]:
         newrow: typing.List[str] = row.copy()
