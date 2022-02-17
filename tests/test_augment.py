1a3307bae880514de575b24845e0a79fe8d70792
GrantXie <67876263+GrantXie@users.noreply.github.com>
2022-02-17 14:47:39 -0800

    Add files via upload

--- tests/test_augment.py
+++ tests/test_augment.py
@@ -16,12 +16,33 @@ class TestKGTKAugment(unittest.TestCase):
     def tearDown(self) -> None:
         shutil.rmtree(self.temp_dir)
 
-    def test_kgtk_add_id_default(self):
-        cli_entry("kgtk", "augment", "-i", self.file_path, "--output-path", self.output_folder_path,
-                  "--verify-id-unique", )
+    def test_kgtk_augment_default(self):
+        cli_entry("kgtk", "augment", "-i", self.file_path, "--output-path", self.output_folder_path)
         df = pd.read_csv(self.file_path, sep='\t')
         files = glob.glob(f'{self.output_folder_path}/*/*.tsv')
 
         for file in files:
             df_temp = pd.read_csv(self.file_path, sep='\t')
             self.assertEqual(len(df), len(df_temp))
+
+    def test_kgtk_augment_lp(self):
+        cli_entry("kgtk", "augment", "--dataset", 'YAGO15K', "--output-path",
+            self.output_folder_path, "--prediction", "lp" )
+        df = pd.read_csv(self.file_path, sep='\t')
+        files = glob.glob(f'{self.output_folder_path}/*/train.tsv')
+
+        for file in files:
+            df_temp = pd.read_csv(self.file_path, sep='\t')
+            self.assertEqual(len(df), len(df_temp))
+
+
+    def test_kgtk_augment_np(self):
+        cli_entry("kgtk", "augment", "--dataset", 'YAGO15K', "--output-path", self.output_folder_path,
+        "--prediction", "np" )
+        df = pd.read_csv(self.file_path, sep='\t')
+        files = glob.glob(f'{self.output_folder_path}/*/train.tsv')
+
+        for file in files:
+            df_temp = pd.read_csv(self.file_path, sep='\t')
+            self.assertEqual(len(df), len(df_temp))
+
